b717e47e2f44322869f2a75043a739ca2bcbf3ef
Daniel <danielwu116@gmail.com>
2021-07-22 17:17:13 -0400

    Solve zipper lists

--- structy.js
+++ structy.js
@@ -720,33 +720,34 @@ const reverseList = (head, prev = null) => {
   return reverseList(nextNode, head);
 };
 
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #16 zipper list ]]]]]]]]]]]]]]]]]]]]]]]]]
 // Write a function, zipperLists, that takes in the head of two linked lists as 
 // arguments. The function should zipper the two lists together into single linked 
 // list by alternating nodes. If one of the linked lists is longer than the other, 
 // the resulting list should terminate with the remaining nodes. The function 
 // should return the head of the zippered linked list.
-
+//
 // Do this in-place, by mutating the original Nodes.
-
+//
 // You may assume that both input lists are non-empty.
-
+//
 // test_00:
 // const a = new Node("a");
 // const b = new Node("b");
 // const c = new Node("c");
 // a.next = b;
 // b.next = c;
-// // a -> b -> c
-
+// a -> b -> c
+//
 // const x = new Node("x");
 // const y = new Node("y");
 // const z = new Node("z");
 // x.next = y;
 // y.next = z;
-// // x -> y -> z
-
+// x -> y -> z
+//
 // zipperLists(a, x);
-// // a -> x -> b -> y -> c -> z
+// a -> x -> b -> y -> c -> z
 // test_01:
 // const a = new Node("a");
 // const b = new Node("b");
@@ -759,23 +760,23 @@ const reverseList = (head, prev = null) => {
 // c.next = d;
 // d.next = e;
 // e.next = f;
-// // a -> b -> c -> d -> e -> f
-
+// a -> b -> c -> d -> e -> f
+//
 // const x = new Node("x");
 // const y = new Node("y");
 // const z = new Node("z");
 // x.next = y;
 // y.next = z;
-// // x -> y -> z
-
+// x -> y -> z
+//
 // zipperLists(a, x);
-// // a -> x -> b -> y -> c -> z -> d -> e -> f
+// a -> x -> b -> y -> c -> z -> d -> e -> f
 // test_02:
 // const s = new Node("s");
 // const t = new Node("t");
 // s.next = t;
-// // s -> t
-
+// s -> t
+//
 // const one = new Node(1);
 // const two = new Node(2);
 // const three = new Node(3);
@@ -783,34 +784,46 @@ const reverseList = (head, prev = null) => {
 // one.next = two;
 // two.next = three;
 // three.next = four;
-// // 1 -> 2 -> 3 -> 4
-
+// 1 -> 2 -> 3 -> 4
+//
 // zipperLists(s, one);
-// // s -> 1 -> t -> 2 -> 3 -> 4
+// s -> 1 -> t -> 2 -> 3 -> 4
 // test_03:
 // const w = new Node("w");
-
-// // w
-
+//
+// w
+//
 // const one = new Node(1);
 // const two = new Node(2);
 // const three = new Node(3);
 // one.next = two;
 // two.next = three;
-// // 1 -> 2 -> 3 
-
+// 1 -> 2 -> 3 
+// 
 // zipperLists(w, one);
-// // w -> 1 -> 2 -> 3
+// w -> 1 -> 2 -> 3
 // test_04:
 // const one = new Node(1);
 // const two = new Node(2);
 // const three = new Node(3);
 // one.next = two;
 // two.next = three;
-// // 1 -> 2 -> 3 
-
+// 1 -> 2 -> 3 
+//
 // const w = new Node("w");
-// // w
-
+// w
+//
 // zipperLists(one, w);
-// // 1 -> w -> 2 -> 3
+// 1 -> w -> 2 -> 3
+
+const zipperLists = (head1, head2) => {
+  if (head1 == null && head2 == null) return null;
+  if (head1 == null) return head2;
+  if (head2 == null) return head1;
+
+  const next1 = head1.next;
+  const next2 = head2.next;
+  head1.next = head2;
+  head2.next = zipperLists(next1, next2);
+  return head1;
+};
