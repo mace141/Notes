eb6f13313cd6e0685f5c4ddf2ebcc6f5d7af027e
Daniel <danielwu116@gmail.com>
2021-07-22 16:33:01 -0400

    Solve get node value

--- structy.js
+++ structy.js
@@ -601,3 +601,75 @@ const linkedListFind = (head, target) => {
 
   return linkedListFind(head.next, target);
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #14 get node value ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function, getNodeValue, that takes in the head of a linked list and 
+// an index. The function should return the value of the linked list at the 
+// specified index.
+//
+// If there is no node at the given index, then return null.
+//
+// test_00:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+//
+// a.next = b;
+// b.next = c;
+// c.next = d;
+//
+// a -> b -> c -> d
+//
+// getNodeValue(a, 2); // 'c'
+// test_01:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+//
+// a.next = b;
+// b.next = c;
+// c.next = d;
+//
+// a -> b -> c -> d
+//
+// getNodeValue(a, 3); // 'd'
+// test_02:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+//
+// a.next = b;
+// b.next = c;
+// c.next = d;
+//
+// a -> b -> c -> d
+//
+// getNodeValue(a, 7); // null
+// test_03:
+// const node1 = new Node("banana");
+// const node2 = new Node("mango");
+//
+// node1.next = node2;
+//
+// banana -> mango
+//
+// getNodeValue(node1, 0); // 'banana'
+// test_04:
+// const node1 = new Node("banana");
+// const node2 = new Node("mango");
+//
+// node1.next = node2;
+//
+// banana -> mango
+//
+// getNodeValue(node1, 1); // 'mango'
+
+const getNodeValue = (head, index) => {
+  if (index == 0) return head.val;
+  if (head == null) return null;
+
+  return getNodeValue(head.next, index - 1);
+};
