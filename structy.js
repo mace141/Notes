69e3e2a99188dc922b180f386c8bdf8024d56ecf
Daniel <danielwu116@gmail.com>
2021-07-23 11:38:34 -0400

    Solve zipperLists iterative

--- structy.js
+++ structy.js
@@ -847,18 +847,43 @@ const reverseList = (head) => { // Time: O(n), Space: O(1)
 // zipperLists(one, w);
 // 1 -> w -> 2 -> 3
 
-// ========================= Recursive =========================
-const zipperLists = (head1, head2) => { // Time: O(min(n, m)), Space: O(min(n, m))
-  if (head1 == null && head2 == null) return null;
-  if (head1 == null) return head2;
-  if (head2 == null) return head1;
+// ========================= Iterative =========================
+const zipperLists = (head1, head2) => { // Time: O(min(n, m)), Space: O(1)
+  let tail = head1;
+  let current1 = head1.next;
+  let current2 = head2;
+  let count = 0;
 
-  const next1 = head1.next;
-  const next2 = head2.next;
-  head1.next = head2;
-  head2.next = zipperLists(next1, next2);
+  while (current1 != null && current2 != null) {
+    if (count % 2 == 0) {
+      tail.next = current2;
+      current2 = current2.next;
+    } else {
+      tail.next = current1;
+      current1 = current1.next;
+    }
+    tail = tail.next;
+    count++;
+  }
+
+  if (current1 == null) tail.next = current2;
+  if (current2 == null) tail.next = current1;
+
   return head1;
 };
+
+// ========================= Recursive =========================
+// const zipperLists = (head1, head2) => { // Time: O(min(n, m)), Space: O(min(n, m))
+//   if (head1 == null && head2 == null) return null;
+//   if (head1 == null) return head2;
+//   if (head2 == null) return head1;
+
+//   const next1 = head1.next;
+//   const next2 = head2.next;
+//   head1.next = head2;
+//   head2.next = zipperLists(next1, next2);
+//   return head1;
+// };
 
 // [[[[[[[[[[[[[[[[[[[[[[[[[ #17 merge lists ]]]]]]]]]]]]]]]]]]]]]]]]] !I
 // Write a function, mergeLists, that takes in the head of two sorted linked 
