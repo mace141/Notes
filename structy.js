dd74f285121e0adc69618d990e15a47e3f8a558f
Daniel <danielwu116@gmail.com>
2021-07-26 16:47:22 -0400

    Solve tree min value

--- structy.js
+++ structy.js
@@ -2132,3 +2132,131 @@ const treeSum = (root) => { // Time: O(n), Space: O(n)
 
   return root.val + treeSum(root.left) + treeSum(root.right);
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #28 tree min value ]]]]]]]]]]]]]]]]]]]]]]]]] 
+// Write a function, treeMinValue, that takes in the root of a binary tree that 
+// contains number values. The function should return the minimum value within 
+// the tree.
+//
+// You may assume that the input tree is non-empty.
+//
+// test_00:
+// const a = new Node(3);
+// const b = new Node(11);
+// const c = new Node(4);
+// const d = new Node(4);
+// const e = new Node(-2);
+// const f = new Node(1);
+//
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+//
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+//
+// treeMinValue(a); // -> -2
+//
+// test_01:
+// const a = new Node(5);
+// const b = new Node(11);
+// const c = new Node(3);
+// const d = new Node(4);
+// const e = new Node(14);
+// const f = new Node(12);
+//
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+//
+//       5
+//    /    \
+//   11     3
+//  / \      \
+// 4   15     12
+//
+// treeMinValue(a); // -> 3
+//
+// test_02:
+// const a = new Node(-1);
+// const b = new Node(-6);
+// const c = new Node(-5);
+// const d = new Node(-3);
+// const e = new Node(-4);
+// const f = new Node(-13);
+// const g = new Node(-2);
+// const h = new Node(-2);
+//
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// f.right = h;
+//
+//        -1
+//      /   \
+//    -6    -5
+//   /  \     \
+// -3   -4   -13
+//     /       \
+//    -2       -2
+//
+// tree_min_value(a); // -> -13
+//
+// test_03:
+// const a = new Node(42);
+//
+//        42
+//
+// tree_min_value(a); // -> 42
+
+// ========================= Depth First Recursive =========================
+const treeMinValue = (root) => { // Time: O(n), Space: O(n)
+  if (root == null) return Infinity;
+
+  const minLeft = treeMinValue(root.left);
+  const minRight = treeMinValue(root.right);
+
+  return Math.min(root.val, minLeft, minRight);
+};
+
+// ========================= Depth First Iterative =========================
+const treeMinValue = (root) => { // Time: O(n), Space: O(n)
+  let minValue = Infinity;
+  const stack = [root];
+
+  while (stack.length) {
+    const node = stack.pop();
+
+    if (node.val < minValue) minValue = node.val;
+    if (node.left != null) stack.push(node.left);
+    if (node.right != null) stack.push(node.right);
+  }
+
+  return minValue;
+};
+
+// ========================= Breadth First =========================
+const treeMinValue = (root) => { // Time: O(n^2), Space: O(n)
+  let minValue = Infinity;
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+
+    if (node.val < minValue) minValue = node.val;
+    if (node.left != null) queue.push(node.left);
+    if (node.right != null) queue.push(node.right);
+  }
+
+  return minValue;
+};
