e5f9dff91a518d16cbb2d59d91fbdbcca3040ef9
Daniel <danielwu116@gmail.com>
2021-07-26 19:56:12 -0400

    Solve how high

--- structy.js
+++ structy.js
@@ -2670,11 +2670,98 @@ const pathFinderHelper = (root, target) => {
 // test_04:
 // treeValueCount(null, 42); // -> 0
 
-const treeValueCount = (root, target) => {
+// ========================= Depth First =========================
+const treeValueCount = (root, target) => { // Time: O(n), Space: O(n)
   if (root == null) return 0;
 
   const leftCount = treeValueCount(root.left, target);
   const rightCount = treeValueCount(root.right, target);
 
   return (root.val == target ? 1 : 0) + leftCount + rightCount;
+};
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #31 how high ]]]]]]]]]]]]]]]]]]]]]]]]] 
+// Write a function, howHigh, that takes in the root of a binary tree. The 
+// function should return a number representing the height of the tree.
+//
+// The height of a binary tree is defined as the maximal number of edges from 
+// the root node to any leaf node.
+//
+// If the tree is empty, return -1.
+//
+// test_00:
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
+//
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+//
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//
+// howHigh(a); // -> 2
+//
+// test_01:
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
+// const g = new Node('g');
+//
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g
+//
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    /
+//   g
+//
+// howHigh(a); // -> 3
+//
+// test_02:
+// const a = new Node('a');
+// const c = new Node('c');
+//
+// a.right = c;
+//
+//      a
+//       \
+//        c
+//
+// howHigh(a); // -> 1
+//
+// test_03:
+// const a = new Node('a');
+//
+//      a
+//
+// howHigh(a); // -> 0
+
+// ========================= Recursive =========================
+const howHigh = (root) => { // Time: O(n), Space: O(n)
+  if (root == null) return -1;
+
+  const leftHeight = howHigh(root.left);
+  const rightHeight = howHigh(root.right);
+
+  return 1 + Math.max(leftHeight, rightHeight);
 };
