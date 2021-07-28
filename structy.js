ca0443b71621f6284c76e47be5238dad5cd223a5
Daniel <danielwu116@gmail.com>
2021-07-28 17:54:15 -0400

    Solve longest path

--- structy.js
+++ structy.js
@@ -4262,3 +4262,80 @@ const closestCarrot = (grid, startRow, startCol) => { // Time: O(rc), Space: O(r
 
   return -1;
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #46 longest path ]]]]]]]]]]]]]]]]]]]]]]]]] 
+// Write a function, longestPath, that takes in an adjacency list for a directed
+// acyclic graph. The function should return the length of the longest path 
+// within the graph. A path may start and end at any two nodes. The length of a
+// path is considered the number of edges in the path, not the number of nodes.
+//
+// test_00:
+// const graph = {
+//   a: ['c', 'b'],
+//   b: ['c'],
+//   c: []
+// };
+//
+// longestPath(graph); // -> 2
+//
+// test_01:
+// const graph = {
+//   a: ['c', 'b'],
+//   b: ['c'],
+//   c: [],
+//   q: ['r'],
+//   r: ['s', 'u', 't'],
+//   s: ['t'],
+//   t: ['u'],
+//   u: []
+// };
+//
+// longestPath(graph); // -> 4
+//
+// test_02:
+// const graph = {
+//   h: ['i', 'j', 'k'],
+//   g: ['h'],
+//   i: [],
+//   j: [],
+//   k: [],
+//   x: ['y'],
+//   y: []
+// };
+//
+// longestPath(graph); // -> 2
+//
+// test_03:
+// const graph = {
+//   a: ['b'],
+//   b: ['c'],
+//   c: [],
+//   e: ['f'],
+//   f: ['g'],
+//   g: ['h'],
+//   h: []
+// };
+//
+// longestPath(graph); // -> 3
+
+const longestPath = (graph) => {
+  let longest = -Infinity;
+
+  for (let start in graph) {
+    const length = pathLength(graph, start);
+    if (length > longest) longest = length;
+  }
+
+  return longest;
+};
+
+const pathLength = (graph, start) => {
+  if (!graph[start].length) return 0;
+
+  const lengths = [];
+  for (let neighbor of graph[start]) {
+    lengths.push(pathLength(graph, neighbor));
+  }
+
+  return 1 + Math.max(...lengths);
+};
