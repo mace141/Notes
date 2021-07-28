ccf685f73e6b022fa44eaeacf644a1b944b31c13
Daniel <danielwu116@gmail.com>
2021-07-27 20:16:52 -0400

    Solve shortest path

--- structy.js
+++ structy.js
@@ -3811,3 +3811,144 @@ const exploreComponent = (graph, node, visited, size = 0) => {
   
   return size;
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #42 shortest path ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function, shortestPath, that takes in an array of edges for an 
+// undirected graph and two nodes (nodeA, nodeB). The function should return the
+// length of the shortest path between A and B. Consider the length as the number
+// of edges in the path, not the number of nodes. If there is no path between A 
+// and B, then return -1.
+//
+// test_00:
+// const edges = [
+//   ['w', 'x'],
+//   ['x', 'y'],
+//   ['z', 'y'],
+//   ['z', 'v'],
+//   ['w', 'v']
+// ];
+//
+// shortestPath(edges, 'w', 'z'); // -> 2
+//
+// test_01:
+// const edges = [
+//   ['w', 'x'],
+//   ['x', 'y'],
+//   ['z', 'y'],
+//   ['z', 'v'],
+//   ['w', 'v']
+// ];
+//
+// shortestPath(edges, 'y', 'x'); // -> 1
+//
+// test_02:
+// const edges = [
+//   ['a', 'c'],
+//   ['a', 'b'],
+//   ['c', 'b'],
+//   ['c', 'd'],
+//   ['b', 'd'],
+//   ['e', 'd'],
+//   ['g', 'f']
+// ];
+//
+// shortestPath(edges, 'a', 'e'); // -> 3
+//
+// test_03:
+// const edges = [
+//   ['a', 'c'],
+//   ['a', 'b'],
+//   ['c', 'b'],
+//   ['c', 'd'],
+//   ['b', 'd'],
+//   ['e', 'd'],
+//   ['g', 'f']
+// ];
+//
+// shortestPath(edges, 'e', 'c'); // -> 2
+//
+// test_04:
+// const edges = [
+//   ['a', 'c'],
+//   ['a', 'b'],
+//   ['c', 'b'],
+//   ['c', 'd'],
+//   ['b', 'd'],
+//   ['e', 'd'],
+//   ['g', 'f']
+// ];
+//
+// shortestPath(edges, 'b', 'g'); // -> -1
+//
+// test_05:
+// const edges = [
+//   ['c', 'n'],
+//   ['c', 'e'],
+//   ['c', 's'],
+//   ['c', 'w'],
+//   ['w', 'e'],
+// ];
+//
+// shortestPath(edges, 'w', 'e'); // -> 1
+//
+// test_06:
+// const edges = [
+//   ['c', 'n'],
+//   ['c', 'e'],
+//   ['c', 's'],
+//   ['c', 'w'],
+//   ['w', 'e'],
+// ];
+//
+// shortestPath(edges, 'n', 'e'); // -> 2
+//
+// test_07:
+// const edges = [
+//   ['m', 'n'],
+//   ['n', 'o'],
+//   ['o', 'p'],
+//   ['p', 'q'],
+//   ['t', 'o'],
+//   ['r', 'q'],
+//   ['r', 's']
+// ];
+//
+// shortestPath(edges, 'm', 's'); // -> 6
+
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  const visited = new Set();
+  const queue = [{ node: nodeA, length: 0 }];
+
+  while (queue.length) {
+    const { node, length } = queue.shift();
+    if (node === nodeB) return length;
+    visited.add(node);
+
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        queue.push({ node: neighbor, length: length + 1 });
+      }
+    }
+  }
+
+  return -1;
+};
+
+const buildGraph = (edges) => {
+  const graph = {};
+  for (let edge of edges) {
+    const [ node1, node2 ] = edge;
+    if (graph[node1]) {
+      graph[node1].push(node2);
+    } else {
+      graph[node1] = [node2];
+    }
+    if (graph[node2]) {
+      graph[node2].push(node1);
+    } else {
+      graph[node2] = [node1];
+    }
+  }
+  return graph;
+};
