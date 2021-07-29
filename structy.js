9ad85a52235102935274f243ac22b6157dbe5680
Daniel <danielwu116@gmail.com>
2021-07-29 19:06:13 -0400

    Solve sum possible

--- structy.js
+++ structy.js
@@ -4996,3 +4996,44 @@ const tribonacci = (n, memo = {}) => {
 
   return memo[n];
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #53 sum possible ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function sumPossible that takes in an amount and an array of positive
+// numbers. The function should return a boolean indicating whether or not it is
+// possible to create the amount by summing numbers of the array. You may reuse
+// numbers of the array as many times as necessary.
+// You may assume that the target amount is non-negative.
+//
+// test_00:
+// sumPossible(8, [5, 12, 4]); // -> true, 4 + 4
+// test_01:
+// sumPossible(15, [6, 2, 10, 19]); // -> false
+// test_02:
+// sumPossible(13, [6, 2, 1]); // -> true
+// test_03:
+// sumPossible(103, [6, 20, 1]); // -> true
+// test_04:
+// sumPossible(12, []); // -> false
+// test_05:
+// sumPossible(12, [12]); // -> true
+// test_06:
+// sumPossible(0, []); // -> true
+// test_07:
+// sumPossible(271, [10, 8, 265, 24]); // -> false
+// test_08:
+// sumPossible(2017, [4, 2, 10]); // -> false
+
+// ========================= Memoization =========================
+const sumPossible = (amount, numbers, memo = {}) => {
+  if (amount === 0) return true;
+  if (amount in memo) return memo[amount];
+  
+  memo[amount] = false;
+  for (let num of numbers) {
+    if (amount - num >= 0 && sumPossible(amount - num, numbers, memo)) {
+      memo[amount] = true;
+    }
+  }
+
+  return memo[amount];
+};
