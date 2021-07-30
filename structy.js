0cb94121a87a27061fda6d6b4a44367cbff40bb1
Daniel <danielwu116@gmail.com>
2021-07-30 11:14:39 -0400

    Solve min change

--- structy.js
+++ structy.js
@@ -5024,16 +5024,62 @@ const tribonacci = (n, memo = {}) => {
 // sumPossible(2017, [4, 2, 10]); // -> false
 
 // ========================= Memoization =========================
+// Time: O(a*n), Space: O(n)
 const sumPossible = (amount, numbers, memo = {}) => {
   if (amount === 0) return true;
   if (amount in memo) return memo[amount];
   
   memo[amount] = false;
   for (let num of numbers) {
-    if (amount - num >= 0 && sumPossible(amount - num, numbers, memo)) {
+    if (num <= amount && sumPossible(amount - num, numbers, memo)) {
       memo[amount] = true;
     }
   }
 
   return memo[amount];
+};
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #53 min change ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function minChange that takes in an amount and an array of coins. The
+// function should return the minimum number of coins required to create the
+// amount. You may use each coin as many times as necessary.
+// If it is not possible to create the amount, then return -1.
+//
+// test_00:
+// minChange(8, [1, 5, 4, 12]); // -> 2, because 4+4 is the minimum coins possible
+// test_01:
+// minChange(13, [1, 9, 5, 14, 30]); // -> 5
+// test_02:
+// minChange(23, [2, 5, 7]); // -> 4
+// test_03:
+// minChange(102, [1, 5, 10, 25]); // -> 6
+// test_04:
+// minChange(200, [1, 5, 10, 25]); // -> 8
+// test_05:
+// minChange(2017, [4, 2, 10]); // -1
+// test_06:
+// minChange(271, [10, 8, 265, 24]); // -1
+// test_07:
+// minChange(0, [4, 2, 10]); // 0
+// test_08:
+// minChange(0, [])); // 0
+
+const minChange = (amount, coins) => {
+  const min = _minChange(amount, coins);
+  return min === Infinity ? -1 : min;
+};
+
+const _minChange = (amount, coins, memo = {}) => {
+  if (amount === 0) return 0;
+  if (amount in memo) return memo[amount];
+
+  let min = Infinity;
+  for (let coin of coins) {
+    if (coin <= amount) {
+      const numCoins = _minChange(amount - coin, coins, memo) + 1;
+      min = Math.min(numCoins, min);
+    }
+  }
+
+  return memo[amount] = min;
 };
