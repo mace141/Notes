cf2540fe735c2d2f74d26364f71b0d24d5c49e67
Daniel <danielwu116@gmail.com>
2021-08-03 17:40:28 -0400

    Solve can concat

--- structy.js
+++ structy.js
@@ -5576,3 +5576,42 @@ const overlapSubsequence = (str1, str2, i = 0, j = 0, memo = {}) => {
 
   return memo[key];
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #63 can concat ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function, canConcat, that takes in a string and an array of words as
+// arguments. The function should return boolean indicating whether or not it is
+// possible to concatenate words of the array together to form the string.
+// You may reuse words of the array as many times as needed.
+//
+// test_00:
+// canConcat("oneisnone", ["one", "none", "is"]); // -> true
+// test_01:
+// canConcat("oneisnone", ["on", "e", "is"]); // -> false
+// test_02:
+// canConcat("oneisnone", ["on", "e", "is", "n"]); // -> true
+// test_03:
+// canConcat("foodisgood", ["is", "g", "ood", "f"]); // -> true
+// test_04:
+// canConcat("santahat", ["santah", "hat"]); // -> false
+// test_05:
+// canConcat("santahat", ["santah", "san", "hat", "tahat"]); // -> true
+// test_06:
+// canConcat("rrrrrrrrrrrrrrrrrrrrrrrrrrx", ["r", "rr", "rrr", "rrrr", "rrrrr", "rrrrrr"]); // -> false
+
+// ========================= Memoization =========================
+// Time: O(sw), Space: O(s)
+const canConcat = (str, words, memo = {}) => {
+  if (str in memo) return memo[str];
+  if (str.length === 0) return true;
+  
+  for (let word of words) {
+    const slicedStr = str.slice(word.length);
+    if (str.startsWith(word) && canConcat(slicedStr, words, memo)) {
+      memo[str] = true;
+      return true;
+    }
+  }
+  
+  memo[str] = false;
+  return memo[str];
+};
