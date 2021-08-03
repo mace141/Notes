d21d41556d844fc1246c11034a47707f7ff3612c
Daniel <danielwu116@gmail.com>
2021-08-03 18:06:24 -0400

    Solve paired parentheses

--- structy.js
+++ structy.js
@@ -5660,3 +5660,43 @@ const quickestConcat = (str, words) => {
   const min = _quickestConcat(str, words);
   return min === Infinity ? -1 : min;
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #65 paired parentheses ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function, pairedParentheses, that takes in a string as an argument.
+// The function should return a boolean indicating whether or not the string has
+// well-formed parentheses.
+// You may assume the string contains only alphabetic characters, '(', or ')'.
+//
+// test_00:
+// pairedParentheses("(david)((abby))"); // -> true
+// test_01:
+// pairedParentheses("()rose(jeff"); // -> false
+// test_02:
+// pairedParentheses(")("); // -> false
+// test_03:
+// pairedParentheses("()"); // -> true
+// test_04:
+// pairedParentheses("(((potato())))"); // -> true
+// test_05:
+// pairedParentheses("(())(water)()"); // -> true
+// test_06:
+// pairedParentheses("(())(water()()"); // -> false
+// test_07:
+// pairedParentheses(""); // -> true
+
+// ========================= Counter =========================
+// Time: O(n), Space: O(1);
+const pairedParentheses = (str) => {
+  let count = 0;
+  
+  for (let char of str) {
+    if (char === '(') {
+      count++;
+    } else if (char === ')') {
+      if (count === 0) return false;
+      count--;
+    }
+  }
+  
+  return !count
+};
