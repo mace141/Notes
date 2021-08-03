0d8f55a2a5534bf604e59ad9807094ddf17f998d
Daniel <danielwu116@gmail.com>
2021-08-02 22:37:24 -0400

    Solve overlap subsequence

--- structy.js
+++ structy.js
@@ -5537,3 +5537,42 @@ const maxPalinSubsequence = (str, i = 0, j = str.length - 1, memo = {}) => {
 
   return memo[key];
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #62 overlap subsequence ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function, overlapSubsequence, that takes in two strings as arguments.
+// The function should return the length of the longest overlapping subsequence.
+// A subsequence of a string can be created by deleting any characters of the
+// string, while maintaining the relative order of characters.
+//
+// test_00:
+// overlapSubsequence("dogs", "daogt"); // -> 3
+// test_01:
+// overlapSubsequence("xcyats", "criaotsi"); // -> 4
+// test_02:
+// overlapSubsequence("xfeqortsver", "feeeuavoeqr"); // -> 5
+// test_03:
+// overlapSubsequence("kinfolklivemustache", "bespokekinfolksnackwave"); // -> 11
+// test_04:
+// overlapSubsequence(
+//   "mumblecorebeardleggingsauthenticunicorn",
+//   "succulentspughumblemeditationlocavore"
+// ); // -> 15
+
+// ========================= Memoization =========================
+// Time: O(nm), Space: O(nm)
+const overlapSubsequence = (str1, str2, i = 0, j = 0, memo = {}) => {
+  const key = i + '-' + j;
+  if (key in memo) return memo[key];
+  if (i === str1.length || j === str2.length) return 0;
+
+  if (str1[i] === str2[j]) {
+    memo[key] = 1 + overlapSubsequence(str1, str2, i + 1, j + 1, memo);
+  } else {
+    memo[key] = Math.max(
+      overlapSubsequence(str1, str2, i + 1, j, memo),
+      overlapSubsequence(str1, str2, i, j + 1, memo)
+    );
+  }
+
+  return memo[key];
+};
