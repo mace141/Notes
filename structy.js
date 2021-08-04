89fceea0a4e6585118379fc54d4d112cab766e33
Daniel <danielwu116@gmail.com>
2021-08-04 16:03:25 -0400

    Solve create combinations

--- structy.js
+++ structy.js
@@ -6004,3 +6004,57 @@ const permutations = (items) => {
 
   return allPerms;
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #71 create combinations ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function, createCombinations, that takes in an array and a length as
+// arguments. The function should return a 2D array representing all of the
+// combinations of the specifized length.
+// The items within the combinations and the combinations themselves may be returned in any order.
+// You may assume that the input array contains unique elements and 1 <= k <= items.length.
+//
+// test_00:
+// createCombinations(["a", "b", "c"], 2); // ->
+// [
+//   [ 'a', 'b' ],
+//   [ 'a', 'c' ],
+//   [ 'b', 'c' ]
+// ]
+// test_01:
+// createCombinations(["q", "r", "s", "t"], 2); // ->
+// [
+//   [ 'q', 'r' ],
+//   [ 'q', 's' ],
+//   [ 'q', 't' ],
+//   [ 'r', 's' ],
+//   [ 'r', 't' ],
+//   [ 's', 't' ]
+// ]
+// test_02:
+// createCombinations(['q', 'r', 's', 't'], 3)); // ->
+// [
+//   [ 'q', 'r', 's' ],
+//   [ 'q', 'r', 't' ],
+//   [ 'q', 's', 't' ],
+//   [ 'r', 's', 't' ]
+// ]
+// test_03:
+// createCombinations([1, 28, 94], 3); // ->
+// [
+//   [ 1, 28, 94 ]
+// ]
+
+// ========================= Recursive =========================
+// Time: O(n! / k!(n-k)!), Space: O(n! / k!(n-k)!)
+// "n choose k" = binomial coefficient
+const createCombinations = (items, k) => {
+  if (k === 0) return [[]];
+  if (k > items.length) return [];
+  if (k === items.length) return items;
+
+  const first = items.slice(0, 1);
+  const rest = items.slice(1);
+  const combos1 = createCombinations(rest, k - 1);
+  const combos2 = createCombinations(rest, k);
+  
+  return combos1.map(combo => first.concat(combo)).push(combos2);
+};
