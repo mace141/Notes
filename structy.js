b96c5dbccd5cc2e6edd4be1276940c666bee7706
Daniel <danielwu116@gmail.com>
2021-08-05 16:18:01 -0400

    Solve middle value

--- structy.js
+++ structy.js
@@ -6343,14 +6343,96 @@ const generate = (words, synonyms) => {
 // test_06:
 // linkedPalindrome(null); // true
 
+// ========================= Array =========================
+// Time: O(n), Space: O(n)
 const linkedPalindrome = (head) => {
-  const stack = [];
+  const values = [];
   let currNode = head;
 
   while (currNode !== null) {
-    stack.push(currNode.val);
+    values.push(currNode.val);
     currNode = currNode.next;
   }
 
-  return stack.join('') === stack.reverse().join('');
+  return values.join('') === values.reverse().join('');
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #75 middle value ]]]]]]]]]]]]]]]]]]]]]]]]] 
+// Write a function, middleValue, that takes in the head of a linked list as an
+// argument. The function should return the value of the middle node in the
+// linked list. If the linked list has an even number of nodes, then return the
+// value of the second middle node.
+// You may assume that the input list is non-empty.
+//
+// test_00:
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+// a -> b -> c -> d -> e
+// middleValue(a); // c
+// test_01:
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+// e.next = f;
+// a -> b -> c -> d -> e -> f
+// middleValue(a); // d
+// test_02:
+// const x = new Node('x');
+// const y = new Node('y');
+// const z = new Node('z');
+// x.next = y;
+// y.next = z;
+// x -> y -> z
+// middleValue(x); // y
+// test_03:
+// const x = new Node('x');
+// const y = new Node('y');
+// x.next = y;
+// x -> y 
+// middleValue(x); // y
+// test_04:
+// const q = new Node('q');
+// q
+// middleValue(q); // q
+
+// ========================= Array =========================
+// Time: O(n), Space: O(n)
+const middleValue = (head) => {
+  const values = [];
+  let currNode = head;
+
+  while (currNode !== null) {
+    values.push(currNode.val);
+    currNode = currNode.next;
+  }
+
+  return values[Math.floor(values.length / 2)];
+};
+
+// ========================= Array =========================
+// Time: O(n), Space: O(1)
+const middleValue = (head) => {
+  let fast = head;
+  let slow = head;
+
+  while (fast.next !== null && fast !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow.val;
+}
