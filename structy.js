ba2751a68704aa0e030c53bc5901fd9e5302d5b7
Daniel <danielwu116@gmail.com>
2021-08-10 12:53:59 -0400

    Solve tolerant teams

--- structy.js
+++ structy.js
@@ -7041,3 +7041,112 @@ const valid = (graph, node, coloring, currentColor) => {
   
   return true;
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #81 tolerant teams ]]]]]]]]]]]]]]]]]]]]]]]]] 
+// Write a function, tolerantTeams, that takes in an array of rivalries as an
+// argument. A rivalry is a pair of people who should not be placed on the same
+// team. The function should return a boolean indicating whether or not it is
+// possible to separate people into two teams, without rivals being on the same
+// team. The two teams formed do not have to be the same size.
+//
+// test_00:
+// tolerantTeams([
+//   ['philip', 'seb'],
+//   ['raj', 'nader']
+// ]); // -> true
+// test_01:
+// tolerantTeams([
+//   ['philip', 'seb'],
+//   ['raj', 'nader'],
+//   ['raj', 'philip'],
+//   ['seb', 'raj']
+// ]); // -> false
+// test_02:
+// tolerantTeams([
+//   ['cindy', 'anj'],
+//   ['alex', 'matt'],
+//   ['alex', 'cindy'],
+//   ['anj', 'matt'],
+//   ['brando', 'matt']
+// ]); // -> true
+// test_03:
+// tolerantTeams([
+//   ['alex', 'anj'],
+//   ['alex', 'matt'],
+//   ['alex', 'cindy'],
+//   ['anj', 'matt'],
+//   ['brando', 'matt']
+// ]); // -> false
+// test_04:
+// tolerantTeams([
+//   ['alan', 'jj'],
+//   ['betty', 'richard'],
+//   ['jj', 'simcha'],
+//   ['richard', 'christine']
+// ]); // -> true
+// test_05:
+// tolerantTeams([
+//   ['alan', 'jj'],
+//   ['betty', 'richard'],
+//   ['jj', 'simcha'],
+//   ['richard', 'christine']
+// ]); // -> true
+// test_06:
+// tolerantTeams([
+//   ['alan', 'jj'],
+//   ['jj', 'richard'],
+//   ['betty', 'richard'],
+//   ['jj', 'simcha'],
+//   ['richard', 'christine']
+// ]); // -> true
+// test_07:
+// tolerantTeams([
+//   ['alan', 'jj'],
+//   ['betty', 'richard'],
+//   ['betty', 'christine'],
+//   ['jj', 'simcha'],
+//   ['richard', 'christine']
+// ]); // -> false
+
+// ========================= Depth First =========================
+// e = number of rivalries, n = number of people
+// Time: O(e), Space: O(n)
+const tolerantTeams = (rivalries) => {
+  const graph = buildGraph(rivalries);
+  const teams = {};
+  
+  for (let player in graph) {
+    if (!(player in teams)) {
+      if (!setTeams(graph, teams, player, true)) return false;
+    }
+  }
+
+  return true;
+};
+
+const buildGraph = (rivalries) => {
+  const graph = {};
+
+  for (let pair of rivalries) {
+    const [ player1, player2 ] = pair;
+
+    if (!(player1 in graph)) graph[player1] = [];
+    if (!(player2 in graph)) graph[player2] = [];
+    graph[player1].push(player2);
+    graph[player2].push(player1);
+  }
+
+  return graph;
+};
+
+const setTeams = (graph, teams, player, whichTeam) => {
+  if (player in teams) return teams[player] === whichTeam;
+
+  teams[player] = whichTeam;
+
+  for (let rival of graph[player]) {
+    if (!setTeams(graph, teams, rival, !whichTeam)) return false;
+  }
+
+  return true;
+};
