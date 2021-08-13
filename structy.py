4a5edbe32f222d99e298f68b24a6dcebba93fccf
Daniel <danielwu116@gmail.com>
2021-08-13 12:44:41 -0400

    Solve is prime python

--- structy.py
+++ structy.py
@@ -1,21 +1,21 @@
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #1 max value ]]]]]]]]]]]]]]]]]]]]]]]]]
-# Write a function, maxValue, that takes in array of numbers as an argument. 
+# Write a function, max_value, that takes in array of numbers as an argument. 
 # The function should return the largest number in the array.
 # Solve this without using any built-in array methods.
 # You can assume that the array is non-empty.
 #
 # test_00:
-# maxValue([4, 7, 2, 8, 10, 9]); # -> 10
+# max_value([4, 7, 2, 8, 10, 9]); # -> 10
 # test_01:
-# maxValue([10, 5, 40, 40.3]); # -> 40.3
+# max_value([10, 5, 40, 40.3]); # -> 40.3
 # test_02:
-# maxValue([-5, -2, -1, -11]); # -> -1
+# max_value([-5, -2, -1, -11]); # -> -1
 # test_03:
-# maxValue([42]); # -> 42
+# max_value([42]); # -> 42
 # test_04:
-# maxValue([1000, 8]); # -> 1000
+# max_value([1000, 8]); # -> 1000
 # test_05:
-# maxValue([1000, 8, 9000]); # -> 9000
+# max_value([1000, 8, 9000]); # -> 9000
 
 def max_value(nums):
   max = float('-inf')
@@ -27,7 +27,7 @@ def max_value(nums):
   return max
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #2 is prime ]]]]]]]]]]]]]]]]]]]]]]]]]
-# Write a function, isPrime, that takes in a number as an argument. The function 
+# Write a function, is_prime, that takes in a number as an argument. The function 
 # should return a boolean indicating whether or not the given number is prime.
 # A prime number is a number that is only divisible two distinct numbers: 1 and itself.
 # For example, 7 is a prime because it is only divisible by 1 and 7. For example, 
@@ -35,31 +35,41 @@ def max_value(nums):
 # You can assume that the input number is a positive integer.
 #
 # test_00:
-# isPrime(2); # -> true
+# is_prime(2); # -> true
 # test_01:
-# isPrime(3); # -> true
+# is_prime(3); # -> true
 # test_02:
-# isPrime(4); # -> false
+# is_prime(4); # -> false
 # test_03:
-# isPrime(5); # -> true
+# is_prime(5); # -> true
 # test_04:
-# isPrime(6); # -> false
+# is_prime(6); # -> false
 # test_05:
-# isPrime(7); # -> true
+# is_prime(7); # -> true
 # test_06:
-# isPrime(8); # -> false
+# is_prime(8); # -> false
 # test_07:
-# isPrime(25); # -> false
+# is_prime(25); # -> false
 # test_08:
-# isPrime(31); # -> true
+# is_prime(31); # -> true
 # test_09:
-# isPrime(2017); # -> true
+# is_prime(2017); # -> true
 # test_10:
-# isPrime(2048); # -> false
+# is_prime(2048); # -> false
 # test_11:
-# isPrime(1); # -> false
+# is_prime(1); # -> false
 
+from math import floor, sqrt
 
+def is_prime(n):
+  if n < 2:
+    return False
+
+  for f in range(2, floor(sqrt(n)) + 1):
+    if n % f == 0:
+      return False
+
+  return True
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #3 uncompress ]]]]]]]]]]]]]]]]]]]]]]]]]
 # Write a function, uncompress, that takes in a string as an argument. The input 
