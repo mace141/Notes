002e00f9baf268f3b400802106df9573ccf4d18f
Daniel <danielwu116@gmail.com>
2021-08-16 13:37:11 -0400

    Solve pair product python

--- structy.py
+++ structy.py
@@ -261,25 +261,33 @@ def pair_sum(numbers, target_sum):
 
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #8 pair product ]]]]]]]]]]]]]]]]]]]]]]]]] !I
-# Write a function, pairProduct, that takes in an array and a target product as
-# arguments. The function should return an array containing a pair of indices 
-# whose elements multiply to the given target. The indices returned must be unique.
+# Write a function, pair_product, that takes in a list and a target product as 
+# arguments. The function should return a tuple containing a pair of indices 
+# whose elements multiply to the given target. The indices returned must be 
+# unique.
 # Be sure to return the indices, not the elements themselves.
 # There is guaranteed to be one such pair whose product is the target.
 #
 # test_00:
-# pairProduct([3, 2, 5, 4, 1], 8); # -> [1, 3]
+# pair_product([3, 2, 5, 4, 1], 8) # -> (1, 3)
 # test_01:
-# pairProduct([3, 2, 5, 4, 1], 10); # -> [1, 2]
+# pair_product([3, 2, 5, 4, 1], 10) # -> (1, 2)
 # test_02:
-# pairProduct([4, 7, 9, 2, 5, 1], 5); # -> [4, 5]
+# pair_product([4, 7, 9, 2, 5, 1], 5) # -> (4, 5)
 # test_03:
-# pairProduct([4, 7, 9, 2, 5, 1], 35); # -> [1, 4]
+# pair_product([4, 7, 9, 2, 5, 1], 35) # -> (1, 4)
 # test_04:
-# pairProduct([3, 2, 5, 4, 1], 10); # -> [1, 2]
+# pair_product([3, 2, 5, 4, 1], 10) # -> (1, 2)
 # test_05:
-# pairProduct([4, 6, 8, 2], 16); # -> [2, 3]
+# pair_product([4, 6, 8, 2], 16) # -> (2, 3)
 
+def pair_product(numbers, target_product):
+  quotients = {}
+  for i, num in enumerate(numbers):
+    quotient = target_product / num
+    if num in quotients:
+      return (quotients[num], i)
+    quotients[quotient] = i
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #9 intersection ]]]]]]]]]]]]]]]]]]]]]]]]]
 # Write a function, intersection, that takes in two arrays, a,b, as arguments. 
