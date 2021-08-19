412a2750f0702025bc606b50595b0bfc7c7f0384
Daniel <danielwu116@gmail.com>
2021-08-19 17:08:31 -0400

    Solve topological order

--- structy.js
+++ structy.js
@@ -8386,3 +8386,73 @@ const lexicalOrder = (word1, word2, alphabet) => {
 
   return word1.length <= word2.length;
 };
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[ #96 topological order ]]]]]]]]]]]]]]]]]]]]]]]]]
+// Write a function, topologicalOrder, that takes in an object representing the
+// adjacency list for a directed-acyclic graph. The function should return an
+// array containing the topological-order of the graph.
+// The topological ordering of a graph is a sequence where "parent nodes" appear
+// before their "children" within the sequence.
+//
+// test_00:
+// topologicalOrder({
+//   a: ["f"],
+//   b: ["d"],
+//   c: ["a", "f"],
+//   d: ["e"],
+//   e: [],
+//   f: ["b", "e"],
+// }); // -> ['c', 'a', 'f', 'b', 'd', 'e']
+// test_01:
+// topologicalOrder({
+//   h: ["l", "m"],
+//   i: ["k"],
+//   j: ["k", "i"],
+//   k: ["h", "m"],
+//   l: ["m"],
+//   m: [],
+// }); // -> ['j', 'i', 'k', 'h', 'l', 'm']
+// test_02:
+// topologicalOrder({
+//   q: [],
+//   r: ["q"],
+//   s: ["r"],
+//   t: ["s"],
+// }); // -> ['t', 's', 'r', 'q']
+// test_03:
+// topologicalOrder({
+//   v: ["z", "w"],
+//   w: [],
+//   x: ["w", "v", "z"],
+//   y: ["x"],
+//   z: ["w"],
+// }); // -> ['y', 'x', 'v', 'z', 'w']
+
+// Time: O(n+e), Space: O(n)
+const topologicalOrder = (graph) => {
+  const numParents = {};
+  for (let node in graph) {
+    numParents[node] = 0;
+  }
+  for (let node in graph) {
+    for (let neighbor of graph[node]) {
+      numParents[neighbor]++;
+    }
+  }
+  
+  const topMost = [];
+  for (let node in numParents) {
+    if (numParents[node] === 0) topMost.push(node);
+  }
+
+  const ordered = [];
+  while (topMost.length) {
+    const node = topMost.pop();
+    ordered.push(node);
+    for (let neighbor of graph[node]) {
+      numParents[neighbor]--;
+      if (numParents[neighbor] === 0) topMost.push(neighbor);
+    }
+  }
+  return ordered;
+};
