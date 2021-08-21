814cd7140ccf8a37cdd0ddc03a14d304b18c8369
Daniel <danielwu116@gmail.com>
2021-08-21 18:14:17 -0400

    Solve bottom right value

--- structy.py
+++ structy.py
@@ -2310,7 +2310,6 @@ def how_high(root, i = 0):
 # Write a function, bottomRightValue, that takes in the root of a binary tree. 
 # The function should return the right-most value in the bottom-most level of 
 # the tree.
-#
 # You may assume that the input tree is non-empty.
 #
 # test_00:
@@ -2320,21 +2319,17 @@ def how_high(root, i = 0):
 # d = new Node(4);
 # e = new Node(-2);
 # f = new Node(1);
-#
 # a.left = b;
 # a.right = c;
 # b.left = d;
 # b.right = e;
 # c.right = f;
-#
 #       3
 #    /    \
 #   11     10
 #  / \      \
 # 4   -2     1
-#
 # bottomRightValue(a); # -> 1
-#
 # test_01:
 # a = new Node(-1);
 # b = new Node(-6);
@@ -2344,7 +2339,6 @@ def how_high(root, i = 0):
 # f = new Node(-13);
 # g = new Node(-2);
 # h = new Node(6);
-#
 # a.left = b;
 # a.right = c;
 # b.left = d;
@@ -2352,7 +2346,6 @@ def how_high(root, i = 0):
 # c.right = f;
 # e.left = g;
 # e.right = h;
-#
 #        -1
 #      /   \
 #    -6    -5
@@ -2360,9 +2353,7 @@ def how_high(root, i = 0):
 # -3   -4   -13
 #     / \       
 #    -2  6
-#
 # bottomRightValue(a); # -> 6
-#
 # test_02:
 # a = new Node(-1);
 # b = new Node(-6);
@@ -2373,7 +2364,6 @@ def how_high(root, i = 0):
 # g = new Node(-2);
 # h = new Node(6);
 # i = new Node(7);
-#
 # a.left = b;
 # a.right = c;
 # b.left = d;
@@ -2382,7 +2372,6 @@ def how_high(root, i = 0):
 # e.left = g;
 # e.right = h;
 # f.left = i;
-#
 #        -1
 #      /   \
 #    -6    -5
@@ -2390,9 +2379,7 @@ def how_high(root, i = 0):
 # -3   -4   -13
 #     / \    /   
 #    -2  6  7 
-#
 # bottomRightValue(a); # -> 7
-#
 # test_03
 # a = new Node('a');
 # b = new Node('b');
@@ -2400,13 +2387,11 @@ def how_high(root, i = 0):
 # d = new Node('d');
 # e = new Node('e');
 # f = new Node('f');
-#
 # a.left = b;
 # a.right = c;
 # b.right = d;
 # d.left = e;
 # e.right = f;
-#
 #      a
 #    /   \ 
 #   b     c
@@ -2418,14 +2403,24 @@ def how_high(root, i = 0):
 # f
 #       
 # bottomRightValue(a); # -> 'f'
-#
 # test_04
 # a = new Node(42);
-#
 #      42
-#
 # bottomRightValue(a); # -> 42
 
+from collections import deque
+
+def bottom_right_value(root):
+  result = None
+  queue = deque([root])
+  while queue:
+    node = queue.popleft()
+    result = node
+    if node.left:
+      queue.append(node.left)
+    if node.right:
+      queue.append(node.right)
+  return result.val
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #34 all tree paths ]]]]]]]]]]]]]]]]]]]]]]]]] !I
 # Write a function, allTreePaths, that takes in the root of a binary tree. The
