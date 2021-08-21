04fe0c84e03646b2c0a3e9d36e602bf7bc876c75
Daniel <danielwu116@gmail.com>
2021-08-21 18:26:19 -0400

    Solve all tree paths

--- structy.py
+++ structy.py
@@ -2307,7 +2307,7 @@ def how_high(root, i = 0):
   return max(left, right)
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #33 bottom right value ]]]]]]]]]]]]]]]]]]]]]]]]] 
-# Write a function, bottomRightValue, that takes in the root of a binary tree. 
+# Write a function, bottom_right_value, that takes in the root of a binary tree. 
 # The function should return the right-most value in the bottom-most level of 
 # the tree.
 # You may assume that the input tree is non-empty.
@@ -2329,7 +2329,7 @@ def how_high(root, i = 0):
 #   11     10
 #  / \      \
 # 4   -2     1
-# bottomRightValue(a); # -> 1
+# bottom_right_value(a); # -> 1
 # test_01:
 # a = new Node(-1);
 # b = new Node(-6);
@@ -2353,7 +2353,7 @@ def how_high(root, i = 0):
 # -3   -4   -13
 #     / \       
 #    -2  6
-# bottomRightValue(a); # -> 6
+# bottom_right_value(a); # -> 6
 # test_02:
 # a = new Node(-1);
 # b = new Node(-6);
@@ -2379,7 +2379,7 @@ def how_high(root, i = 0):
 # -3   -4   -13
 #     / \    /   
 #    -2  6  7 
-# bottomRightValue(a); # -> 7
+# bottom_right_value(a); # -> 7
 # test_03
 # a = new Node('a');
 # b = new Node('b');
@@ -2402,11 +2402,11 @@ def how_high(root, i = 0):
 #  /
 # f
 #       
-# bottomRightValue(a); # -> 'f'
+# bottom_right_value(a); # -> 'f'
 # test_04
 # a = new Node(42);
 #      42
-# bottomRightValue(a); # -> 42
+# bottom_right_value(a); # -> 42
 
 from collections import deque
 
@@ -2423,13 +2423,11 @@ def bottom_right_value(root):
   return result.val
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #34 all tree paths ]]]]]]]]]]]]]]]]]]]]]]]]] !I
-# Write a function, allTreePaths, that takes in the root of a binary tree. The
+# Write a function, all_tree_paths, that takes in the root of a binary tree. The
 # function should return a 2-Dimensional list where each sublist represents a
 # root-to-leaf path in the tree.
-#
 # The order within an individual path must start at the root and end at the leaf,
 # but the relative order of among paths in the outer list does not matter.
-#
 # You may assume that the input tree is non-empty.
 #
 # test_00:
@@ -2439,26 +2437,22 @@ def bottom_right_value(root):
 # d = new Node('d');
 # e = new Node('e');
 # f = new Node('f');
-#
 # a.left = b;
 # a.right = c;
 # b.left = d;
 # b.right = e;
 # c.right = f;
-#
 #      a
 #    /   \
 #   b     c
 #  / \     \
 # d   e     f
-#
-# allTreePaths(a); # ->
+# all_tree_paths(a); # ->
 # [ 
 #   [ 'a', 'b', 'd' ], 
 #   [ 'a', 'b', 'e' ], 
 #   [ 'a', 'c', 'f' ] 
 # ] 
-#
 # test_01:
 # a = new Node('a');
 # b = new Node('b');
@@ -2469,7 +2463,6 @@ def bottom_right_value(root):
 # g = new Node('g');
 # h = new Node('h');
 # i = new Node('i');
-#
 # a.l#eft = b;
 # a.right = c;
 # b.left = d;
@@ -2478,7 +2471,6 @@ def bottom_right_value(root):
 # e.left = g;
 # e.right = h;
 # f.left = i;
-#
 #         a
 #      /    \
 #     b      c
@@ -2486,15 +2478,13 @@ def bottom_right_value(root):
 #  d    e      f
 #      / \    /   
 #     g  h   i 
-#
-# allTreePaths(a); # ->
+# all_tree_paths(a); # ->
 # [ 
 #   [ 'a', 'b', 'd' ], 
 #   [ 'a', 'b', 'e', 'g' ], 
 #   [ 'a', 'b', 'e', 'h' ], 
 #   [ 'a', 'c', 'f', 'i' ] 
 # ] 
-#
 # test_02:
 # q = new Node('q');
 # r = new Node('r');
@@ -2502,13 +2492,11 @@ def bottom_right_value(root):
 # t = new Node('t');
 # u = new Node('u');
 # v = new Node('v');
-#
 # q.left = r;
 # q.right = s;
 # r.right = t;
 # t.left = u;
 # u.right = v;
-#
 #      q
 #    /   \ 
 #   r     s
@@ -2518,22 +2506,36 @@ def bottom_right_value(root):
 #   u
 #  /
 # v
-#
-# allTreePaths(q); # ->
+# all_tree_paths(q); # ->
 # [ 
 #   [ 'q', 'r', 't', 'u', 'v' ], 
 #   [ 'q', 's' ] 
 # ] 
-#
 # test_03:
 # z = new Node('z');
-#
 #      z
-#
-# console.log(allTreePaths(z)); # -> 
+# all_tree_paths(z); # -> 
 # [
 #   ['z']
 # ]
+
+def all_tree_paths(root):
+  if root is None:
+    return []
+  if root.left is None and root.right is None:
+    return [[root.val]]
+  
+  paths = []
+
+  left_paths = all_tree_paths(root.left)
+  for path in left_paths:
+    paths.append([root.val, *path])
+
+  right_paths = all_tree_paths(root.right)
+  for path in right_paths:
+    paths.append([root.val, *path])
+    
+  return paths
 
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #35 tree levels ]]]]]]]]]]]]]]]]]]]]]]]]] 
