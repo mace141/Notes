5941ee750b11132a9340e35b30c31565efcaa63f
Daniel <danielwu116@gmail.com>
2021-08-25 18:27:51 -0400

    Solve best bridge py

--- structy.py
+++ structy.py
@@ -3707,7 +3707,7 @@ def longest_education(graph, course):
   return max
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #48 best bridge ]]]]]]]]]]]]]]]]]]]]]]]]] !I
-# Write a function, bestBridge, that takes in a grid as an argument. The grid 
+# Write a function, best_bridge, that takes in a grid as an argument. The grid 
 # contains water (W) and land (L). There are exactly two islands in the grid. 
 # An island is a vertically or horizontally connected region of land. Return 
 # the minimum length bridge needed to connect the two islands. A bridge does not
@@ -3722,7 +3722,7 @@ def longest_education(graph, course):
 #   ["W", "W", "W", "W", "W"],
 #   ["W", "W", "W", "W", "W"],
 # ]
-# bestBridge(grid) # -> 1
+# best_bridge(grid) # -> 1
 # test_01:
 # grid = [
 #   ["W", "W", "W", "W", "W"],
@@ -3732,14 +3732,14 @@ def longest_education(graph, course):
 #   ["W", "W", "W", "L", "L"],
 #   ["W", "W", "W", "W", "W"],
 # ]
-# bestBridge(grid) # -> 2
+# best_bridge(grid) # -> 2
 # test_02:
 # grid = [
 #   ["W", "W", "W", "W", "W"],
 #   ["W", "W", "W", "L", "W"],
 #   ["L", "W", "W", "W", "W"],
 # ]
-# bestBridge(grid) # -> 3
+# best_bridge(grid) # -> 3
 # test_03:
 # grid = [
 #   ["W", "W", "W", "W", "W", "W", "W", "W"],
@@ -3753,7 +3753,7 @@ def longest_education(graph, course):
 #   ["L", "L", "L", "W", "W", "W", "W", "W"],
 #   ["W", "W", "W", "W", "W", "W", "W", "W"],
 # ]
-# bestBridge(grid) # -> 3
+# best_bridge(grid) # -> 3
 # test_04:
 # grid = [
 #   ["L", "L", "L", "L", "L", "L", "L", "L"],
@@ -3769,7 +3769,7 @@ def longest_education(graph, course):
 #   ["L", "W", "W", "W", "W", "W", "W", "L"],
 #   ["L", "L", "L", "L", "L", "L", "L", "L"],
 # ]
-# bestBridge(grid) # -> 2
+# best_bridge(grid) # -> 2
 # test_05:
 # grid = [
 #   ["W", "L", "W", "W", "W", "W", "W", "W"],
@@ -3781,8 +3781,63 @@ def longest_education(graph, course):
 #   ["W", "W", "W", "W", "W", "W", "L", "L"],
 #   ["W", "W", "W", "W", "W", "W", "W", "L"],
 # ]
-# bestBridge(grid) # -> 8
+# best_bridge(grid) # -> 8
 
+from collections import deque
+
+def best_bridge(grid):
+  first_island = set()
+  land_positions = []
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):
+      if grid[r][c] == 'L':
+        scope_out_land(grid, land_positions, first_island, r, c)
+        break
+    else:
+      continue 
+    break
+
+  bridge_length = float('inf')
+  for pos in land_positions:
+    y, x = pos
+    length = bridge(grid, first_island, y, x)
+    bridge_length = length if length < bridge_length else bridge_length
+  return bridge_length
+
+def scope_out_land(grid, land_positions, first_island, r, c):
+  if (r, c) in first_island or grid[r][c] == 'W':
+    return
+  first_island.add((r, c))
+  land_positions.append([r, c])
+  deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  for delta in deltas:
+    [new_row, new_col], valid_pos = in_bounds(grid, delta, r, c)
+    if valid_pos:
+      scope_out_land(grid, land_positions, first_island, new_row, new_col)
+
+def bridge(grid, first_island, row, col):
+  deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  visited = set()
+  queue = deque([[row, col, -1]])
+  while queue:
+    y, x, dst = queue.popleft()
+    if grid[y][x] == 'L' and (y, x) not in first_island:
+      return dst
+    visited.add((y, x))
+    for delta in deltas:
+      [new_row, new_col], valid_pos = in_bounds(grid, delta, y, x)
+      pos = (new_row, new_col)
+      if valid_pos and pos not in visited and pos not in first_island:
+        queue.append([new_row, new_col, dst + 1])
+  return float('inf')
+
+def in_bounds(grid, delta, row, col):
+  delta_y, delta_x = delta
+  new_row = row + delta_y
+  new_col = col + delta_x
+  valid_row = new_row >= 0 and new_row < len(grid)
+  valid_col = new_col >= 0 and new_col < len(grid[0])
+  return [[new_row, new_col], valid_row and valid_col]
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #49 has cycle ]]]]]]]]]]]]]]]]]]]]]]]]] !I
 # Write a function, hasCycle, that takes in an object representing the adjacency
--- test.py
+++ test.py
@@ -1,71 +1,119 @@
-def semesters_required(num_courses, prereqs):
-  graph = build_graph(num_courses, prereqs)
-  semesters = 1
-  for course in graph:
-    length = longest_path(graph, course)
-    semesters = length if length > semesters else semesters
-  return semesters
+from collections import deque
 
-def build_graph(num_courses, prereqs):
-  graph = {}
-  for num in range(num_courses):
-    graph[num] = []
-  for pair in prereqs:
-    prereq, course = pair
-    graph[prereq].append(course)
-  return graph
+def best_bridge(grid):
+  first_island = set()
+  land_positions = []
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):
+      if grid[r][c] == 'L':
+        scope_out_land(grid, land_positions, first_island, r, c)
+        break
+    else:
+      continue 
+    break
 
-def longest_path(graph, course):
-  if not graph[course]:
-    return 1
-  max = 0
-  for next_course in graph[course]:
-    length = longest_path(graph, next_course)
-    max = length if length > max else max
-  return 1 + max
+  bridge_length = float('inf')
+  for pos in land_positions:
+    y, x = pos
+    length = bridge(grid, first_island, y, x)
+    bridge_length = length if length < bridge_length else bridge_length
+  return bridge_length
 
-num_courses = 6
-prereqs = [
-  [1, 2],
-  [2, 4],
-  [3, 5],
-  [0, 5],
+def scope_out_land(grid, land_positions, first_island, r, c):
+  if (r, c) in first_island or grid[r][c] == 'W':
+    return
+  first_island.add((r, c))
+  land_positions.append([r, c])
+  deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  for delta in deltas:
+    [new_row, new_col], valid_pos = in_bounds(grid, delta, r, c)
+    if valid_pos:
+      scope_out_land(grid, land_positions, first_island, new_row, new_col)
+
+def bridge(grid, first_island, row, col):
+  deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  visited = set()
+  queue = deque([[row, col, -1]])
+  while queue:
+    y, x, dst = queue.popleft()
+    if grid[y][x] == 'L' and (y, x) not in first_island:
+      return dst
+    visited.add((y, x))
+    for delta in deltas:
+      [new_row, new_col], valid_pos = in_bounds(grid, delta, y, x)
+      pos = (new_row, new_col)
+      if valid_pos and pos not in visited and pos not in first_island:
+        queue.append([new_row, new_col, dst + 1])
+  return float('inf')
+
+def in_bounds(grid, delta, row, col):
+  delta_y, delta_x = delta
+  new_row = row + delta_y
+  new_col = col + delta_x
+  valid_row = new_row >= 0 and new_row < len(grid)
+  valid_col = new_col >= 0 and new_col < len(grid[0])
+  return [[new_row, new_col], valid_row and valid_col]
+
+grid = [
+  ["W", "W", "W", "L", "L"],
+  ["L", "L", "W", "W", "L"],
+  ["L", "L", "L", "W", "L"],
+  ["W", "L", "W", "W", "W"],
+  ["W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W"],
 ]
-print(semesters_required(num_courses, prereqs)) # -> 3
-num_courses = 7
-prereqs = [
-  [4, 3],
-  [3, 2],
-  [2, 1],
-  [1, 0],
-  [5, 2],
-  [5, 6],
+print(best_bridge(grid)) # -> 1
+grid = [
+  ["W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W"],
+  ["L", "L", "W", "W", "L"],
+  ["W", "L", "W", "W", "L"],
+  ["W", "W", "W", "L", "L"],
+  ["W", "W", "W", "W", "W"],
 ]
-print(semesters_required(num_courses, prereqs)) # -> 5
-num_courses = 5
-prereqs = [
-  [1, 0],
-  [3, 4],
-  [1, 2],
-  [3, 2],
+print(best_bridge(grid)) # -> 2
+grid = [
+  ["W", "W", "W", "W", "W"],
+  ["W", "W", "W", "L", "W"],
+  ["L", "W", "W", "W", "W"],
 ]
-print(semesters_required(num_courses, prereqs)) # -> 2
-num_courses = 12
-prereqs = []
-print(semesters_required(num_courses, prereqs)) # -> 1
-num_courses = 3
-prereqs = [
-  [0, 2],
-  [0, 1],
-  [1, 2],
+print(best_bridge(grid)) # -> 3
+grid = [
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "L", "W", "W"],
+  ["W", "W", "W", "W", "L", "L", "W", "W"],
+  ["W", "W", "W", "W", "L", "L", "L", "W"],
+  ["W", "W", "W", "W", "W", "L", "L", "L"],
+  ["L", "W", "W", "W", "W", "L", "L", "L"],
+  ["L", "L", "L", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
 ]
-print(semesters_required(num_courses, prereqs)) # -> 3
-num_courses = 6
-prereqs = [
-  [3, 4],
-  [3, 0],
-  [3, 1],
-  [3, 2],
-  [3, 5],
+print(best_bridge(grid)) # -> 3
+grid = [
+  ["L", "L", "L", "L", "L", "L", "L", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "L", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "L", "L", "L", "L", "L", "L", "L"],
 ]
-print(semesters_required(num_courses, prereqs)) # -> 2
+print(best_bridge(grid)) # -> 2
+grid = [
+  ["W", "L", "W", "W", "W", "W", "W", "W"],
+  ["W", "L", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "L", "W"],
+  ["W", "W", "W", "W", "W", "W", "L", "L"],
+  ["W", "W", "W", "W", "W", "W", "W", "L"],
+]
+print(best_bridge(grid)) # -> 8
