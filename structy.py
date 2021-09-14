8f3ee4ea6acd9d5566326d44f843b796c7b34b23
Daniel <danielwu116@gmail.com>
2021-09-14 17:27:32 -0400

    Solve combined intervals py

--- structy.py
+++ structy.py
@@ -6437,7 +6437,6 @@ def _breaking_boundaries(m, n, k, r, c, memo):
   memo[(r, c)] = count
   return count
 
-<<<<<<< HEAD
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #86 merge sort ]]]]]]]]]]]]]]]]]]]]]]]]] 
 # Write a function, merge_sort, that takes in a list of numbers as an argument. 
 # The function should return return a new list containing elements of the 
@@ -6519,8 +6518,98 @@ def merge(left, right):
 
   return res
 
-=======
->>>>>>> b43a71eccb03173a7667c0da69d140bb72764249
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #87 combine intervals ]]]]]]]]]]]]]]]]]]]]]]]]] 
+# Write a function, combine_intervals, that takes in an a list of intervals as an 
+# argument. Each interval is a tuple containing a pair of numbers representing a 
+# start and end time. Your function should combine overlapping intervals and 
+# return a list containing the combined intervals.
+# For example:
+# Given two intervals:
+# (1, 4) and (3, 7)
+# The intervals overlap and
+# should be combined into:
+# (1, 7)
+# You may return the combined intervals in any order.
+# You can assume that the input list contains at least one interval and all intervals are valid with start < end.
+#
+# test_00
+# intervals = [
+#   (1, 4),
+#   (12, 15),
+#   (3, 7),
+#   (8, 13),
+# ]
+# combine_intervals(intervals)
+# # -> [ (1, 7), (8, 15) ]
+# test_01
+# intervals = [
+#   (6, 8),
+#   (2, 9),
+#   (10, 12),
+#   (20, 24),
+# ]
+# combine_intervals(intervals)
+# # -> [ (2, 9), (10, 12), (20, 24) ]
+# test_02
+# intervals = [
+#   (3, 7),
+#   (5, 8),
+#   (1, 5),
+# ]
+# combine_intervals(intervals)
+# # -> [ (1, 8) ]
+# test_03
+# intervals = [
+#   (3, 7),
+#   (10, 13),
+#   (5, 8),
+#   (27, 31),
+#   (1, 5),
+#   (12, 16),
+#   (20, 22),
+# ]
+# combine_intervals(intervals)
+# # -> [ (1, 8), (10, 16), (20, 22), (27, 31) ]
+# test_04
+# intervals = [
+#   (3, 7),
+#   (10, 13),
+#   (5, 8),
+#   (27, 31),
+#   (1, 5),
+#   (12, 16),
+#   (20, 32),
+# ]
+# combine_intervals(intervals)
+# # -> [ (1, 8), (10, 16), (20, 32) ]
+# test_05
+# intervals = [
+#   (64, 70),
+#   (50, 55),
+#   (62, 65),
+#   (12, 50),
+#   (72, 300000),
+# ]
+# combine_intervals(intervals)
+# # -> [ (12, 55), (62, 70), (72, 300000) ]
+
+# ========================= Sort and Combine =========================
+# Time: O(n * log(n)), Space: O(n)
+def combine_intervals(intervals):
+  sorted_intervals = sorted(intervals)
+  combined = [sorted_intervals[0]]
+
+  for interval in sorted_intervals[1:]:
+    curr_start, curr_end = interval 
+    last_start, last_end = combined[-1]
+    if curr_start <= last_end:
+      if curr_end > last_end:
+        combined[-1] = (last_start, curr_end)
+    else:
+      combined.append(interval)
+  
+  return combined
+
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #88 binary search ]]]]]]]]]]]]]]]]]]]]]]]]] 
 # Write a function, binary_search, that takes in a sorted list of numbers and a 
 # target. The function should return the index where the target can be found 
