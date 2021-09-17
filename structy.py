6480618e278e9b72b2ae0c6799c2b51ae565fd4e
Daniel <danielwu116@gmail.com>
2021-09-17 13:47:13 -0400

    Solve lowest common ancestor py

--- structy.py
+++ structy.py
@@ -5566,6 +5566,109 @@ def linked_list_cycle(head):
     fast = fast.next.next
   return False
 
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #77 lowest common ancestor ]]]]]]]]]]]]]]]]]]]]]]]]] 
+# Write a function, lowest_common_ancestor, that takes in the root of a binary
+# tree and two values. The function should return the value of the lowest common
+# ancestor of the two values in the tree.
+# You may assume that the tree values are unique and the tree is non-empty.
+# Note that a node may be considered an ancestor of itself.
+# Example Tree:
+# const a = new Node('a');
+# const b = new Node('b');
+# const c = new Node('c');
+# const d = new Node('d');
+# const e = new Node('e');
+# const f = new Node('f');
+# const g = new Node('g');
+# const h = new Node('h');
+# a.left = b;
+# a.right = c;
+# b.left = d;
+# b.right = e;
+# c.right = f;
+# e.left = g;
+# e.right = h;
+#      a
+#    /    \
+#   b      c
+#  / \      \
+# d   e      f
+#    / \
+#    g  h
+#
+# test_00
+# lowest_common_ancestor(a, 'd', 'h'); # b
+# test_01
+# lowest_common_ancestor(a, 'd', 'g'); # b
+# test_02
+# lowest_common_ancestor(a, 'g', 'c'); # a
+# test_03
+# lowest_common_ancestor(a, 'b', 'g'); # b
+# test_04
+# lowest_common_ancestor(a, 'f', 'c'); # c
+# example tree
+# const l = new Node('l');
+# const m = new Node('m');
+# const n = new Node('n');
+# const o = new Node('o');
+# const p = new Node('p');
+# const q = new Node('q');
+# const r = new Node('r');
+# const s = new Node('s');
+# const t = new Node('t');
+# l.left = m;
+# l.right = n;
+# n.left = o;
+# n.right = p;
+# o.left = q;
+# o.right = r;
+# p.left = s;
+# p.right = t;
+#        l
+#     /     \
+#    m       n
+#         /    \
+#         o     p
+#        / \   / \
+#       q   r s   t
+# test_05
+# lowest_common_ancestor(l, 'r', 'p'); # n
+# test_06
+# lowest_common_ancestor(l, 'm', 'o'); # l
+# test_07
+# lowest_common_ancestor(l, 't', 'q'); # n
+# test_08
+# lowest_common_ancestor(l, 's', 'p'); # p
+
+# ========================= DFS =========================
+# Time: O(n), Space: O(n)
+def lowest_common_ancestor(root, val1, val2):
+  path1 = build_path(root, val1)  
+  path2 = build_path(root, val2)
+  set1 = set(path1)
+
+  for val in path2:
+    if val in set1:
+      return val
+
+def build_path(root, val):
+  if root is None:
+    return None
+  if root.val == val:
+    return [root.val]
+  
+  left_path = build_path(root.left, val)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path
+  
+  right_path = build_path(root.right, val)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+
+  return None
+
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #83 max increasing subseq ]]]]]]]]]]]]]]]]]]]]]]]]] 
 # Write a function, max_increasing_subseq, that takes in a list of numbers as an 
 # argument. The function should return the length of the longest subsequence of 
