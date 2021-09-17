94a441e860df0710c1cb08b960c31976e5090740
Daniel <danielwu116@gmail.com>
2021-09-17 13:17:03 -0400

    Solve linked list cycle py

--- structy.py
+++ structy.py
@@ -5474,6 +5474,98 @@ def middle_value(head):
     fast = fast.next.next
   return slow.val
 
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #76 linked list cycle ]]]]]]]]]]]]]]]]]]]]]]]]] 
+# Write a function, linked_list_cycle, that takes in the head of a linked list 
+# as an argument. The function should return a boolean indicating whether or not 
+# the linked list contains a cycle.
+#
+# test_00:
+# a = Node('a')
+# b = Node('b')
+# c = Node('c')
+# d = Node('d')
+# a.next = b
+# b.next = c
+# c.next = d
+# d.next = b # cycle
+#         _______
+#       /        \
+# a -> b -> c -> d 
+# linked_list_cycle(a)  # True
+# test_01:
+# q = Node('q')
+# r = Node('r')
+# s = Node('s')
+# t = Node('t')
+# u = Node('u')
+# q.next = r
+# r.next = s
+# s.next = t
+# t.next = u
+# u.next = q # cycle
+#    ________________
+#  /                 \
+# q -> r -> s -> t -> u 
+# linked_list_cycle(q)  # True
+# test_02
+# a = Node('a')
+# b = Node('b')
+# c = Node('c')
+# d = Node('d')
+# a.next = b
+# b.next = c
+# c.next = d
+
+# a -> b -> c -> d 
+# linked_list_cycle(a)  # False
+# test_03:
+# q = Node('q')
+# r = Node('r')
+# s = Node('s')
+# t = Node('t')
+# u = Node('u')
+# q.next = r
+# r.next = s
+# s.next = t
+# t.next = u
+# u.next = t # cycle
+#                   __
+#                 /   \
+# q -> r -> s -> t -> u 
+# linked_list_cycle(q)  # True
+# test_04:
+# p = Node('p')
+# p
+# linked_list_cycle(p) # False
+# test_05:
+# linked_list_cycle(None) # False
+
+# ========================= Set =========================
+# Time: O(n), Space: O(n)
+def linked_list_cycle(head):
+  visited = set()
+  current = head 
+  while current is not None:
+    if current.val in visited:
+      return True
+    visited.add(current.val)
+    current = current.next
+  return False
+
+# ========================= Two Pointers =========================
+# Time: O(n), Space: O(1)
+def linked_list_cycle(head):
+  first_iteration = True
+  slow = head 
+  fast = head 
+  while fast is not None and fast.next is not None:
+    if slow is fast and not first_iteration:
+      return True
+    first_iteration = False
+    slow = slow.next
+    fast = fast.next.next
+  return False
+
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #83 max increasing subseq ]]]]]]]]]]]]]]]]]]]]]]]]] 
 # Write a function, max_increasing_subseq, that takes in a list of numbers as an 
 # argument. The function should return the length of the longest subsequence of 
