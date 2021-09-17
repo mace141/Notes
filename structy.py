f82352a7988c76dc54ecdf0c7dcddd68049b200d
Daniel <danielwu116@gmail.com>
2021-09-17 14:10:25 -0400

    Solve lefty nodes py

--- structy.py
+++ structy.py
@@ -5640,7 +5640,7 @@ def linked_list_cycle(head):
 # test_08
 # lowest_common_ancestor(l, 's', 'p'); # p
 
-# ========================= DFS =========================
+# ========================= Depth First =========================
 # Time: O(n), Space: O(n)
 def lowest_common_ancestor(root, val1, val2):
   path1 = build_path(root, val1)  
@@ -5780,7 +5780,7 @@ def build_path(root, val):
 #     /   \
 #    c     y
 
-# ========================= DFS =========================
+# ========================= Depth First =========================
 # Time: O(n), Space: O(n)
 def flip_tree(root):
   if root is None:
@@ -5790,6 +5790,126 @@ def flip_tree(root):
   root.left = flip_tree(right)
   root.right = flip_tree(left)
   return root
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #79 lefty nodes ]]]]]]]]]]]]]]]]]]]]]]]]] 
+# Write a function, lefty_nodes, that takes in the root of a binary tree. The
+# function should return an array containing the left-most value on every level
+# of the tree. The array must be ordered in a top-down fashion where the root
+# is the first element.
+#
+# test_00
+# const a = new Node('a');
+# const b = new Node('b');
+# const c = new Node('c');
+# const d = new Node('d');
+# const e = new Node('e');
+# const f = new Node('f');
+# const g = new Node('g');
+# const h = new Node('h');
+# a.left = b;
+# a.right = c;
+# b.left = d;
+# b.right = e;
+# c.right = f;
+# e.left = g;
+# e.right = h;
+#      a
+#    /    \
+#   b      c
+#  / \      \
+# d   e      f
+#    / \
+#    g  h
+# lefty_nodes(a); # [ 'a', 'b', 'd', 'g' ]
+# test_01
+# const u = new Node('u');
+# const t = new Node('t');
+# const s = new Node('s');
+# const r = new Node('r');
+# const q = new Node('q');
+# const p = new Node('p');
+# u.left = t;
+# u.right = s;
+# s.right = r;
+# r.left = q;
+# r.right = p;
+#     u
+#  /    \
+# t      s
+#         \
+#         r
+#        / \
+#        q  p
+# lefty_nodes(u); # [ 'u', 't', 'r', 'q' ]
+# test_02
+# const l = new Node('l');
+# const m = new Node('m');
+# const n = new Node('n');
+# const o = new Node('o');
+# const p = new Node('p');
+# const q = new Node('q');
+# const r = new Node('r');
+# const s = new Node('s');
+# const t = new Node('t');
+# l.left = m;
+# l.right = n;
+# n.left = o;
+# n.right = p;
+# o.left = q;
+# o.right = r;
+# p.left = s;
+# p.right = t;
+#        l
+#     /     \
+#    m       n
+#         /    \
+#         o     p
+#        / \   / \
+#       q   r s   t
+# lefty_nodes(l); # [ 'l', 'm', 'o', 'q' ]
+# test_03
+# const n = new Node('n');
+# const y = new Node('y');
+# const c = new Node('c');
+# n.left = y;
+# n.right = c;
+#       n
+#     /   \
+#    y     c
+# lefty_nodes(n); # [ 'n', 'y' ]
+# test_04
+# const i = new Node('i');
+# const n = new Node('n');
+# const s = new Node('s');
+# const t = new Node('t');
+# i.right = n;
+# n.left = s;
+# n.right = t;
+#       i
+#        \
+#         n
+#        / \
+#       s   t
+# lefty_nodes(i); # [ 'i', 'n', 's' ]
+# test_05
+# lefty_nodes(None); # [ ]
+
+# ========================= Breadth First =========================
+# Time: O(n), Space: O(n)
+def lefty_nodes(root):
+  leftys = []
+  if root is None:
+    return []
+  queue = deque([(root, 0)])
+  while len(queue):
+    node, level = queue.popleft()
+    if len(leftys) == level:
+      leftys.append(node.val)
+    if node.left:
+      queue.append((node.left, level + 1))
+    if node.right:
+      queue.append((node.right, level + 1))
+  return leftys
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ #83 max increasing subseq ]]]]]]]]]]]]]]]]]]]]]]]]] 
 # Write a function, max_increasing_subseq, that takes in a list of numbers as an 
