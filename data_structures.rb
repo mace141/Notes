ebc6e75526932625310158f30dcbb3884ce14aee
Daniel <danielwu116@gmail.com>
2021-09-21 18:41:00 -0400

    Add min heap to data_structure.rb

--- data_structures.rb
+++ data_structures.rb
@@ -1,11 +1,11 @@
 # [[[[[[[[[[[[[[[[[[[[[[[[[ Binary Trees ]]]]]]]]]]]]]]]]]]]]]]]]]
 
 # Definition
-#   A tree is a graph that does not contain any cycles
-#   A binary tree is a tree where nodes have at most 2 children
-#     An empty graph
-#     A single node
-#     A linked list
+#     A tree is a graph that does not contain any cycles
+#     A binary tree is a tree where nodes have at most 2 children
+#         An empty graph
+#         A single node
+#         A linked list
 
 class BiTreeNode
   def initialize(val)
@@ -113,27 +113,36 @@ def sorted_array_to_bst(nums)
   return root
 end
 
-# [[[[[[[[[[[[[[[[[[[[[[[[[ Max Heap ]]]]]]]]]]]]]]]]]]]]]]]]]
+# [[[[[[[[[[[[[[[[[[[[[[[[[ Binary Heaps ]]]]]]]]]]]]]]]]]]]]]]]]]
+
+# Binary heap is a type of binary tree with partially sorted data. It is always
+#   balanced and complete. Binary heaps are commonly used as priority queues. 
+
+# Time complexity:
+#     Insertion: O(n * log(n))
+#     Extraction: O(n * log(n))
+
+# ========================= Max Heap =========================
 
 class MaxHeap
-  attr_accessor :array
   def initialize
-    @array = [nil]
+    @array = []
   end
   
-  def get_parent(i)
-    i / 2 
+  def parent(i)
+    (i - 1) / 2 
   end
   
   def insert(n)
-    self.array << n
-    self.sift_up(self.array.length - 1)
+    @array << n
+    self.sift_up(@array.length - 1)
   end
   
   def sift_up(i)
-    return if i = 1
-    arr = self.array
-    parent_idx = self.get_parent(i)
+    return if i == 0
+    
+    arr = @array
+    parent_idx = self.parent(i)
     parent = arr[parent_idx]
     current = arr[i]
     if parent < current
@@ -143,19 +152,19 @@ class MaxHeap
   end
   
   def get_max
-    return nil if self.array.length == 1
-    return self.array.pop if self.array.length == 2
+    return nil if @array.length == 0
+    return @array.pop if @array.length == 1
     
-    max = self.array[1]
-    self.array[1] = self.array.pop
-    self.sift_down(1)
+    max = @array[0]
+    @array[0] = @array.pop
+    self.sift_down(0)
     return max
   end
   
   def sift_down(i)
-    arr = self.array
-    left_idx = i * 2
-    right_idx = i * 2 + 1
+    arr = @array
+    left_idx = i * 2 + 1
+    right_idx = i * 2 + 2
     left_child = arr[left_idx]
     right_child = arr[right_idx]
     
@@ -170,14 +179,66 @@ class MaxHeap
   end
 end
 
-arr = Array.new(10) { rand(0..20) }
-p arr
-max = MaxHeap.new
-for n in arr
-  max.insert(n)
+# ========================= Min Heap =========================
+
+class MinHeap
+  def initialize
+    @array = []
+  end
+
+  def parent(i)
+    (i - 1) / 2
+  end
+
+  def left(i)
+    i * 2 + 1
+  end
+
+  def right(i)
+    i * 2 + 2
+  end
+
+  def insert(n)
+    @array << n
+    sift_up(@array.length - 1)
+  end
+
+  def sift_up(i)
+    return if i == 0
+
+    arr = @array
+    parent_idx = parent(i)
+    if arr[parent_idx] > arr[i]
+      arr[i], arr[parent_idx] = arr[parent_idx], arr[i]
+      sift_up(parent_idx)
+    end
+  end
+
+  def get_min
+    return nil if @array.length == 0
+    return @array.pop if @array.length == 1
+    
+    min = @array[0]
+    @array[0] = @array.pop
+    sift_down(0)
+    return min 
+  end
+
+  def sift_down(i)
+    arr = @array
+    current = arr[i]
+    left_idx = left(i)
+    right_idx = right(i)
+    left_child = arr[left_idx]
+    right_child = arr[right_idx]
+
+    left_child = 1.0/0 if left_child.nil?
+    right_child = 1.0/0 if right_child.nil?
+
+    return if current < left_child && current < right_child 
+
+    swap_idx = left_child < right_child ? left_idx : right_idx 
+    arr[i], arr[swap_idx] = arr[swap_idx], arr[i]
+    sift_down(swap_idx)
+  end
 end
-p max.array
-p max.get_max
-p max.get_max
-p max.get_max
-p max.get_max
