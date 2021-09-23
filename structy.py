41192b27fef09c0bd9d4325ba9ae864e0d71fdb9
Daniel <danielwu116@gmail.com>
2021-09-23 11:07:05 -0400

    Solve build tree in post py

--- structy.py
+++ structy.py
@@ -6976,3 +6976,75 @@ def _post_order(root, values):
   _post_order(root.left)
   _post_order(root.right)
   values.append(root.val)
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #92 build tree in post  ]]]]]]]]]]]]]]]]]]]]]]]]] 
+# Write a function, build_tree_in_post, that takes in a list of in-ordered values 
+# and a list of post-ordered values for a binary tree. The function should build 
+# a binary tree that has the given in-order and post-order traversal structure. 
+# The function should return the root of this tree.
+# You can assume that the values of inorder and postorder are unique.
+#
+# test_00
+# build_tree_in_post(
+#   [ 'y', 'x', 'z' ],
+#   [ 'y', 'z', 'x' ] 
+# )
+#       x
+#    /    \
+#   y      z
+# test_01
+# build_tree_in_post(
+#   [ 'd', 'b', 'e', 'a', 'f', 'c', 'g' ],
+#   [ 'd', 'e', 'b', 'f', 'g', 'c', 'a' ] 
+# )
+#      a
+#    /    \
+#   b      c
+#  / \    / \
+# d   e  f   g
+# test_02
+# build_tree_in_post(
+#   [ 'd', 'b', 'g', 'e', 'h', 'a', 'c', 'f' ],
+#   [ 'd', 'g', 'h', 'e', 'b', 'f', 'c', 'a' ] 
+# )
+#      a
+#    /    \
+#   b      c
+#  / \      \
+# d   e      f
+#    / \
+#    g  h
+# test_03
+# build_tree_in_post(
+#   ['m', 'n'],
+#   ['m', 'n']
+# )
+#       n
+#     /
+#    m
+# test_04
+# build_tree_in_post(
+#   ['n', 'm'],
+#   ['m', 'n']
+# )
+#     n
+#      \
+#       m
+
+# ========================= Recursive =========================
+# Time: O(n^2), Space: O(n^2)
+def build_tree_in_post(in_order, post_order):
+  if len(post_order) == 0:
+    return None
+  
+  root_val = post_order[-1]
+  left_length = in_order.index(root_val)
+  left_in_order = in_order[:left_length]
+  right_in_order = in_order[(left_length + 1):]
+  left_post_order = post_order[:left_length]
+  right_post_order = post_order[left_length:-1]
+
+  root = Node(root_val)
+  root.left = build_tree_in_post(left_in_order, left_post_order)
+  root.right = build_tree_in_post(right_in_order, right_post_order)
+  return root
