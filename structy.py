f70d0c0aa2bf52ac31c905dbceb83b735dd5db4a
Daniel <danielwu116@gmail.com>
2021-09-23 13:35:51 -0400

    Solve topological sort py

--- structy.py
+++ structy.py
@@ -7240,3 +7240,68 @@ def lexical_order(word_1, word_2, alphabet):
         return True
   
   return len(word_1) <= len(word_2)
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #96 topological order  ]]]]]]]]]]]]]]]]]]]]]]]]] !I
+# Write a function, topological_order, that takes in a dictionary representing 
+# the adjacency list for a directed-acyclic graph. The function should return a 
+# list containing the topological-order of the graph.
+# The topological ordering of a graph is a sequence where "parent nodes" appear 
+# before their "children" within the sequence.
+#
+# test_00:
+# topological_order({
+#   "a": ["f"],
+#   "b": ["d"],
+#   "c": ["a", "f"],
+#   "d": ["e"],
+#   "e": [],
+#   "f": ["b", "e"],
+# }) # -> ['c', 'a', 'f', 'b', 'd', 'e']
+# test_01:
+# topological_order({
+#   "h": ["l", "m"],
+#   "i": ["k"],
+#   "j": ["k", "i"],
+#   "k": ["h", "m"],
+#   "l": ["m"],
+#   "m": [],
+# }) # -> ['j', 'i', 'k', 'h', 'l', 'm']
+# test_02:
+# topological_order({
+#   "q": [],
+#   "r": ["q"],
+#   "s": ["r"],
+#   "t": ["s"],
+# }) # -> ['t', 's', 'r', 'q']
+# test_03:
+# topological_order({
+#   "v": ["z", "w"],
+#   "w": [],
+#   "x": ["w", "v", "z"],
+#   "y": ["x"],
+#   "z": ["w"],
+# }) # -> ['y', 'x', 'v', 'z', 'w']
+
+# ========================= Kahn's Algorithm =========================
+# n: num nodes, e: num edges
+# Time: O(n + e), Space: O(n)
+def topological_order(graph):
+  num_parents = {}
+  for node in graph:
+    num_parents[node] = 0
+  
+  for node in graph:
+    for child in graph[node]:
+      num_parents[child] += 1
+  
+  top_most = [ node for node in graph if num_parents[node] == 0 ]
+  order = []
+  while top_most:
+    node = top_most.pop()
+    order.append(node)
+    for child in graph[node]:
+      num_parents[child] -= 1
+      if num_parents[child] == 0:
+        top_most.append(child)
+  
+  return order
