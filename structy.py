9eddd0ed456fa7883d7a8936ea2e48cb20f45c29
Daniel <danielwu116@gmail.com>
2021-09-23 11:33:14 -0400

    Solve build tree in pre py

--- structy.py
+++ structy.py
@@ -7031,7 +7031,7 @@ def _post_order(root, values):
 #      \
 #       m
 
-# ========================= Recursive =========================
+# ========================= Recursive (Slicing) =========================
 # Time: O(n^2), Space: O(n^2)
 def build_tree_in_post(in_order, post_order):
   if len(post_order) == 0:
@@ -7047,4 +7047,89 @@ def build_tree_in_post(in_order, post_order):
   root = Node(root_val)
   root.left = build_tree_in_post(left_in_order, left_post_order)
   root.right = build_tree_in_post(right_in_order, right_post_order)
+  return root
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #93 build tree in pre  ]]]]]]]]]]]]]]]]]]]]]]]]] 
+# Write a function, build_tree_in_pre, that takes in a list of in-ordered values 
+# and a list of pre-ordered values for a binary tree. The function should build 
+# a binary tree that has the given in-order and pre-order traversal structure. 
+# The function should return the root of this tree.
+# You can assume that the values of inorder and preorder are unique.
+#
+# test_00
+# build_tree_in_pre(
+#   [ 'z', 'y', 'x' ],
+#   [ 'y', 'z', 'x' ] 
+# )
+#       y
+#    /    \
+#   z      x
+# test_01
+# build_tree_in_pre(
+#   [ 'y', 'z', 'x' ],
+#   [ 'y', 'x', 'z' ] 
+# )
+#       y
+#        \
+#         x
+#        / 
+#       z
+# test_02
+# build_tree_in_pre(
+#   [ 'd', 'b', 'g', 'e', 'h', 'a', 'c', 'f' ],
+#   [ 'a', 'b', 'd', 'e', 'g', 'h', 'c', 'f' ] 
+# )
+#       a
+#    /    \
+#   b      c
+#  / \      \
+# d   e      f
+#    / \
+#    g  h
+# test_03
+# build_tree_in_pre(
+#   [ 't', 'u', 's', 'q', 'r', 'p' ],
+#   [ 'u', 't', 's', 'r', 'q', 'p' ] 
+# )
+#     u
+#  /    \
+# t      s
+#         \
+#         r
+#        / \
+#        q  p
+# test_04
+# build_tree_in_pre(
+#   [ 'm', 'l', 'q', 'o', 'r', 'n', 's', 'p', 't' ],
+#   [ 'l', 'm', 'n', 'o', 'q', 'r', 'p', 's', 't' ] 
+# )
+#        l
+#     /     \
+#    m       n
+#         /    \
+#         o     p
+#        / \   / \
+#       q   r s   t
+
+# ========================= Recursive (In-Place) =========================
+# Time: O(n), Space: O(n)
+def build_tree_in_pre(in_order, pre_order):
+  return _build_tree(in_order, pre_order, 0, len(in_order) - 1, 0, len(pre_order) - 1)
+
+def _build_tree(in_order, pre_order, in_start, in_end, pre_start, pre_end):
+  if in_start > in_end:
+    return None
+    
+  root_val = pre_order[pre_start]
+  mid = in_order.index(root_val)
+  left_length = mid - in_start
+  left_in_end = mid - 1
+  right_in_start = mid + 1
+  left_pre_start = pre_start + 1
+  left_pre_end = pre_start + left_length
+  right_pre_start = pre_start + left_length + 1
+  
+  root = Node(root_val)
+  root.left = _build_tree(in_order, pre_order, in_start, left_in_end, left_pre_start, left_pre_end)
+  root.right = _build_tree(in_order, pre_order, right_in_start, in_end, right_pre_start, pre_end)
   return root
