a2738df443b6817b1483435804504860c5e361bb
Daniel <danielwu116@gmail.com>
2021-09-23 11:59:08 -0400

    Solve lexical order py

--- structy.py
+++ structy.py
@@ -7133,3 +7133,54 @@ def _build_tree(in_order, pre_order, in_start, in_end, pre_start, pre_end):
   root.left = _build_tree(in_order, pre_order, in_start, left_in_end, left_pre_start, left_pre_end)
   root.right = _build_tree(in_order, pre_order, right_in_start, in_end, right_pre_start, pre_end)
   return root
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ #94 lexical order  ]]]]]]]]]]]]]]]]]]]]]]]]] 
+# Write a function, lexical_order, that takes in 2 words and an alphabet string 
+# as an argument. The function should return True if the first word should appear 
+# before the second word if lexically-ordered according to the given alphabet 
+# order. If the second word should appear first, then return False.
+# Note that the alphabet string may be any arbitrary string.
+# Intuitively, Lexical Order is like "dictionary" order:
+# You can assume that all characters are lowercase a-z.
+# You can assume that the alphabet contains all 26 letters.
+#
+# test_00:
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# lexical_order("apple", "dock", alphabet) # -> True
+# test_01:
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# lexical_order("apple", "ample", alphabet) # -> False
+# test_02:
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# lexical_order("app", "application", alphabet) # -> True
+# test_03:
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# lexical_order("backs", "backdoor", alphabet) # -> False
+# test_04:
+# alphabet = "ghzstijbacdopnfklmeqrxyuvw"
+# lexical_order("zoo", "dinner", alphabet) # -> True
+# test_05:
+# alphabet = "ghzstijbacdopnfklmeqrxyuvw"
+# lexical_order("leaper", "leap", alphabet) # -> False
+# test_06:
+# alphabet = "ghzstijbacdopnfklmeqrxyuvw"
+# lexical_order("backs", "backdoor", alphabet) # -> True
+# test_07:
+# alphabet = "ghzstijbacdopnfklmeqrxyuvw"
+# lexical_order("semper", "semper", alphabet) # -> True
+
+# ========================= Iterative =========================
+# Time: O(n), Space: O(1)
+def lexical_order(word_1, word_2, alphabet):
+  for i in range(min(len(word_1), len(word_2))):
+    char_1 = word_1[i]
+    char_2 = word_2[i]
+    if char_1 == char_2:
+      pass 
+    else:
+      if alphabet.index(char_1) > alphabet.index(char_2):
+        return False 
+      else:
+        return True
+  
+  return len(word_1) <= len(word_2)
