786bdf4612a11726a0955d88073ace458ec6c7b3
Daniel <danielwu116@gmail.com>
2021-09-25 13:55:05 -0400

    Update python notes

--- Python/w1.py
+++ Python/w1.py
@@ -262,9 +262,9 @@ for num in range(1,16):
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ User Input ]]]]]]]]]]]]]]]]]]]]]]]]]
 
-print("Hello World!")
-answer = input("How are you?\n")
-print("I am fine")
+# print("Hello World!")
+# answer = input("How are you?\n")
+# print("I am fine")
 
 # [[[[[[[[[[[[[[[[[[[[[[[[[ Built-in Data Types ]]]]]]]]]]]]]]]]]]]]]]]]]
 
@@ -318,7 +318,7 @@ newer_set = set('abracadabra')  # => {'a', 'b', 'r', 'c', 'd'}
 # sorted(iterable, key=None, reverse=False)
 #   creates a new sorted list from the items in the iterable
 #   key: optional function which converts item to a value for comparison
-#        e.g. key=str.lower()
+#        e.g. key=str.lower
 #   reverse: optional boolean
 #   `key` and `reverse` parameters must be set with the name and equal sign
 
@@ -361,6 +361,15 @@ newer_set = set('abracadabra')  # => {'a', 'b', 'r', 'c', 'd'}
 # dir(dictionary)
 #   returns a list of keys in the dictionary
 
+# dict.values(), dict.keys(), dict.items()
+#   creates a list-like object for iteration
+abcs = {'a':1, 'b':2, 'c':3}
+for k in abcs.keys():
+  print(k)
+print('easy as')
+for v in abcs.values():
+  print(v)
+
 # ========================= Sets =========================
 
 a = {1, 2, 3}
@@ -385,3 +394,125 @@ print(b - a)        # => {4, 6}
 #   The `^` operator or symmetric_difference(*sets) will return a new set of items 
 #     that appear in EXACTLY ONE set but NOT the others
 print(a ^ b)        # => {1, 3, 4, 6}
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ Extra Arguments ]]]]]]]]]]]]]]]]]]]]]]]]]
+
+# Functions may accept extra arguments using the `*` operator
+def add(a, b, *args):
+  total = a + b 
+  for n in args:
+    total += n
+  return total
+
+add(1, 2, 3, 4)   # => 10
+
+# To add keyword arguments use the `**` operator. Keyword arguments are 
+#   conventionally named kwargs
+def greet_from_country(greeting, **kwargs):
+  for k, v in kwargs.items():
+    print(greeting, k, 'from', v)
+
+greet_from_country('Hi', 
+                   Michelle='Sweden',
+                   Daniel='Brooklyn')
+# => Hi Michelle from Sweden
+#    Hi Daniel from Brooklyn
+
+# Function arguments must appear in this order:
+#     formal positional arguments
+#     *args
+#     keyword arguments with default values
+#     **kwargs
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ Importing ]]]]]]]]]]]]]]]]]]]]]]]]]
+
+# ========================= Terms =========================
+
+# A module is simply Python code in a separate file.
+# A package is the path to a directory that contains modules which is also a special type of module.
+# __init__.py is the default file for a package.
+# A submodule is another file in a module’s folder.
+# A function is (obviously!) a function in a module.
+
+# ------------------------- Visualization -------------------------
+
+# project
+# │   README.md
+# │   __init__.py
+# |   shopping_cart.py         <== module
+# │
+# └───pet                      <== package
+# │   │
+# │   └───mammal               <== module (and package)
+# |   |   |   __init__.py
+# │   |   │   dog.py           <== submodule
+# │   |   │   cat.py           <== submodule
+# │   |   │   ...
+# │   │
+# │   └───fish                 <== module (and package)
+# |   |   |   __init__.py
+# │   │
+# │   └───bird                 <== module (and package)
+# |       |   __init__.py
+# │
+# └───housing                  <== module (and package)
+#     │   __init__.py
+#     │   aquarium.py          <== submodule
+#     │   cage.py              <== submodule
+#     │   kennel.py            <== submodule
+#     |   ...
+
+# ========================= Import Statements =========================
+
+# Common examples:
+# most basic
+#     import <module> 
+# dot syntax
+#     import <package>.<subpackage>.<module> 
+# one module in a package
+#     from <package> import <module> 
+# multiple modules or submodules in a package
+#     from <package> import <module>, <module> 
+# special case for module's __init__.py to get submodules in the same folder
+#     from . import <submodule>
+# down to the function level
+#     from <module> import <function>, <function> 
+# renaming to avoid confusion or conflict
+#     from <package> import <module> as <altName> 
+
+# When you have large modules, it is good practice to break up functionality into 
+# submodules - that is, separate files - and have __init__.py import them. This 
+# means the module can be imported as a whole, or each part imported as it's 
+# required. These submodules can even be placed into subfolders (and given their 
+# own __init__.py file).
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[ Classes ]]]]]]]]]]]]]]]]]]]]]]]]]
+
+class AngryBird:
+  __slots__ = ['_x', '_y']
+
+  def __init__(self, x=0, y=0):
+    """
+    Construct a new AngryBird with position (0, 0).
+    """
+    self._x = x
+    self._y = y
+
+  def move_up_by(self, dy):
+    self._y += dy
+
+  def get_y(self):
+    return self._y
+
+# In Python, every instance method gets a reference to the object as the first 
+#   parameter
+
+# Under the hood, Python uses a dictionary to store the instance variables of a 
+#   class. You can create a dunder class variable `__slots__` to reserve memory
+#   for the iVars you know you will use. This speeds up Python's instance 
+#   initialization
+
+bird = AngryBird()
+print(bird.get_y())
+bird.move_up_by(2)
+print(bird.get_y())
