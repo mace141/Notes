934e9e13513f6b71c47e2fecdf5d42c6d0d20f11
Daniel <danielwu116@gmail.com>
2021-09-27 22:44:57 -0400

    Solve LC 15

--- problems.py
+++ problems.py
@@ -14,3 +14,22 @@ def plusOne(digits):
     digits.insert(0, 1)
       
   return digits
+
+# ========================= LeetCode 15 =========================
+
+def three_sum(nums):
+  res = set()
+  nums.sort()
+  for i, num1 in enumerate(nums):
+    diff_1 = 0 - num1
+    two = {}
+    j = i + 1
+    while j < len(nums):
+      n = nums[j]
+      if n in two:
+        sorted_lst = sorted([num1, two[n], n])
+        res.add(tuple(sorted_lst))
+      diff_2 = diff_1 - n
+      two[diff_2] = n
+      j += 1
+  return res
