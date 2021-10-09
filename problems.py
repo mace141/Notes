ea3d525b43afc0d1a349501555b3fced3ed916fd
Daniel <danielwu116@gmail.com>
2021-10-09 13:39:23 -0400

    Solve LC 79

--- problems.py
+++ problems.py
@@ -299,3 +299,33 @@ def rotate(matrix):
   for i in range(len(matrix)):
     for j in range(len(matrix) // 2):
       matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+
+# ========================= LeetCode 79 =========================
+
+def exist(board, word):
+  for i in range(len(board)):
+    for j in range(len(board[0])):
+      if search(board, i, j, word, 0, set()):
+        return True
+
+  return False
+
+def search(board, r, c, word, i, visited):
+  if i == len(word):
+    return True
+  if (r, c) in visited:
+    return False
+  if r == len(board) or c == len(board[0]):
+    return False
+  if r < 0 or c < 0:
+    return False
+  
+  if board[r][c] == word[i]:
+    visited.add((r, c))
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    for dy, dx in deltas:
+      if search(board, r + dy, c + dx, word, i + 1, visited):
+        return True
+    visited.remove((r, c))
+
+  return False
--- test.py
+++ test.py
@@ -1,13 +1,36 @@
-def jump(nums):
-  start = reach = jumps = 0
-  
-  while reach < len(nums) - 1:
-    print(start, reach) 
-    furthest = max(i + nums[i] for i in range(start, reach + 1))
-    start, reach = reach, furthest
-    jumps += 1
-    
-  print(start, reach) 
-  print(jumps)
+import pdb
+pdb.set_trace()
 
-jump([1, 1, 1, 1, 1, 1])
+def exist(board, word):
+  for i in range(len(board)):
+    for j in range(len(board[0])):
+      if search(board, i, j, word, 0, set()):
+        return True
+
+  return False
+
+def search(board, r, c, word, i, visited):
+  if i == len(word):
+    return True
+  if (r, c) in visited:
+    return False
+  if r == len(board) or c == len(board[0]):
+    return False
+  if r < 0 or c < 0:
+    return False
+  
+  if board[r][c] == word[i]:
+    visited.add((r, c))
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    for dy, dx in deltas:
+      if search(board, r + dy, c + dx, word, i + 1, visited):
+        return True
+    visited.remove((r, c))
+
+  return False
+
+board = [["A","B","C","E"],
+         ["S","F","E","S"],
+         ["A","D","E","E"]]
+word = "ABCEFSADEESE"
+print(exist(board, word))
