81b5a1ba3deaebd3510065f262a099a58b5e7adb
Daniel <danielwu116@gmail.com>
2021-10-21 18:29:45 -0400

    Solve LC 26

--- Data Structures & Algorithms/problems.py
+++ Data Structures & Algorithms/problems.py
@@ -444,3 +444,15 @@ def findTargetSumWays(nums, target):
     memo[(i, total)] = count
     return count
   return helper(nums, target, 0, 0, {})
+
+# ========================= LeetCode 26 =========================
+
+def removeDuplicates(nums):
+  i = 0
+  j = 1
+  while j < len(nums):
+    if nums[i] != nums[j]:
+      i += 1
+      nums[i] = nums[j]
+    j += 1
+  return i + 1
