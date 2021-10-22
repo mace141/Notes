e235cd7876126d573ae842ad4675d8c5b5c07fd5
Daniel <danielwu116@gmail.com>
2021-10-22 16:12:12 -0400

    Solve LC 344

--- Data Structures & Algorithms/problems.py
+++ Data Structures & Algorithms/problems.py
@@ -554,3 +554,14 @@ def isValidSudoku(board):
         return False
   
   return True
+
+# ========================= LeetCode 344 =========================
+
+def reverse_string(s):
+  i = 0
+  j = len(s) - 1
+  while i < j:
+    s[i], s[j] = s[j], s[i]
+    i += 1
+    j -= 1
+
